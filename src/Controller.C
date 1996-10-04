518067cd8494cbaaec0de3a36b138dc2bc7d835e
Jim Phillips <jim@ks.uiuc.edu>
1996-10-04 16:09:30 -0500

    switched intenting

--- src/Controller.C
+++ src/Controller.C
@@ -16,31 +16,31 @@
 
 void Sequencer_threadRun(Sequencer* arg)
 {
-	arg->threadRun();
+    arg->threadRun();
 }
 
 void Sequencer::run(int numberOfCycles)
 {
-	this->numberOfCycles = numberOfCycles;
-	thread = CthCreate((CthVoidFn)&(Sequencer_threadRun),(void*)(this),0);
-	CthSetStrategyDefault(thread);
-	CthAwaken(thread);
+    this->numberOfCycles = numberOfCycles;
+    thread = CthCreate((CthVoidFn)&(Sequencer_threadRun),(void*)(this),0);
+    CthSetStrategyDefault(thread);
+    CthAwaken(thread);
 }
 
 void Sequencer::threadRun(void)
 {
-	const int stepsPerCycle = 2;
-	const int timestep = 1.0;
-	int step, cycle;
-	for ( cycle = 0; cycle < numberOfCycles; ++cycle )
-	{
-		for ( step = 0; step < stepsPerCycle; ++step )
-		{
-			// patch->addForceToMomentum(0.5*timestep);
-			// patch->addVelocityToPosition(timestep);
-			// patch->positionsReady();
-			suspend();
-			// patch->addForceToMomentum(0.5*timestep);
-		}
-	}
+    const int stepsPerCycle = 2;
+    const int timestep = 1.0;
+    int step, cycle;
+    for ( cycle = 0; cycle < numberOfCycles; ++cycle )
+    {
+        for ( step = 0; step < stepsPerCycle; ++step )
+        {
+            // patch->addForceToMomentum(0.5*timestep);
+            // patch->addVelocityToPosition(timestep);
+            // patch->positionsReady();
+            suspend();
+            // patch->addForceToMomentum(0.5*timestep);
+        }
+    }
 }
--- src/Sequencer.h
+++ src/Sequencer.h
@@ -21,20 +21,20 @@ class HomePatch;
 class Sequencer
 {
 public:
-	Sequencer(HomePatch *p) : patch(p) { };
-	~Sequencer(void) { };
-	void run(int numberOfCycles);             // spawn thread, etc.
-	void awaken(void) { CthAwaken(thread); };
-	void suspend(void) { CthSuspend(); };
+    Sequencer(HomePatch *p) : patch(p) { };
+    ~Sequencer(void) { };
+    void run(int numberOfCycles);             // spawn thread, etc.
+    void awaken(void) { CthAwaken(thread); };
+    void suspend(void) { CthSuspend(); };
 
 protected:
-	virtual void threadRun(void);  // subclasses redefine this method
-	int numberOfCycles;            // stores argument to run()
-	HomePatch *const patch;        // access methods in patch
+    virtual void threadRun(void);  // subclasses redefine this method
+    int numberOfCycles;            // stores argument to run()
+    HomePatch *const patch;        // access methods in patch
 
 private:
-	CthThread thread;
-	friend void Sequencer_threadRun(Sequencer*);
+    CthThread thread;
+    friend void Sequencer_threadRun(Sequencer*);
 };
 
 #endif // SEQUENCER_H
