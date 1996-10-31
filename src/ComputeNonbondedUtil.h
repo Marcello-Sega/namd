583d858724d544656dad23c7d29e5cc7fddf8a1e
Jim Phillips <jim@ks.uiuc.edu>
1996-10-31 16:35:04 -0600

    Initial revision

---
+++ src/ComputeNonbondedUtil.h
@@ -0,0 +1,44 @@
+/***************************************************************************/
+/*                                                                         */
+/*              (C) Copyright 1996 The Board of Trustees of the            */
+/*                          University of Illinois                         */
+/*                           All Rights Reserved                           */
+/*									   */
+/***************************************************************************/
+
+/***************************************************************************
+ * DESCRIPTION: Common operations for ComputeNonbonded classes
+ *
+ ***************************************************************************/
+
+#ifndef COMPUTENONBONDEDUTIL_H
+#define COMPUTENONBONDEDUTIL_H
+
+class ComputeNonbondedUtil {
+
+public:
+
+protected:
+
+private:
+
+};
+
+#endif
+/***************************************************************************
+ * RCS INFORMATION:
+ *
+ *	$RCSfile: ComputeNonbondedUtil.h,v $
+ *	$Author: jim $	$Locker:  $		$State: Exp $
+ *	$Revision: 1.1 $	$Date: 1996/10/31 22:35:04 $
+ *
+ ***************************************************************************
+ * REVISION HISTORY:
+ *
+ * $Log: ComputeNonbondedUtil.h,v $
+ * Revision 1.1  1996/10/31 22:35:04  jim
+ * Initial revision
+ *
+ *
+ ***************************************************************************/
+
