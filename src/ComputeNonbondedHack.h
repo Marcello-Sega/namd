fecb858751d778aa810409acffaab9ba23bfd034
Jim Phillips <jim@ks.uiuc.edu>
1996-11-06 17:03:44 -0600

    changed format slightly, getting better

--- src/ComputeNonbondedHack.h
+++ src/ComputeNonbondedHack.h
@@ -13,23 +13,41 @@
 
 // Several special cases are defined:
 //   NBPAIR, NBSELF, NBEXCL switch environment (mutually exclusive)
-//   EXCLUDE14 modified 1-4 parameters?
+//   MODIFY14 modified 1-4 parameters?
 //   SWITCHING switching function?
-//   FULLELECT full electrostatics?
 
-// Check mutual exclusivity
 #ifdef NBPAIR
+#define PAIR(X) X
+#else
+#define PAIR(X)
+#endif
+
 #ifdef NBSELF
-#error Error!
+#define SELF(X) X
+#else
+#define SELF(X)
 #endif
+
 #ifdef NBEXCL
-#error Error!
+#define EXCL(X) X
+#else
+#define EXCL(X)
 #endif
+
+#ifdef MODIFY14
+#define M14(X) X
+#define NOM14(X)
+#else
+#define M14(X)
+#define NOM14(X) X
 #endif
-#ifdef NBSELF
-#ifdef NBEXCL
-#error Error!
-#endif
+
+#ifdef SWITCHING
+#define SW(X) X
+#define NOSW(X)
+#else
+#define SW(X)
+#define NOSW(X) X
 #endif
 
 #if defined NBPAIR
@@ -52,30 +70,23 @@
 
 (Position* p PLEN, Force* f PLEN, AtomProperties* a PLEN)
 {
-  Vector f_elec;			//  Electrostatic force vector
-  Vector f_vdw;				//  Vdw force vector
-					//  same as vector from atom2 to atom1
-  BigReal r;				//  Distance between current pair of
-					//  atoms
-  BigReal r2;				//  r squared
-  BigReal r_1;				//  1/r
-  BigReal r_6, r_12;			//  1/r^6 and 1/r^12
-   
   BigReal electEnergy = 0;
   BigReal vdwEnergy = 0;
 
   const Real cutoff = Node::Object()->simParameters->cutoff;
   const BigReal cutoff2 = cutoff*cutoff;
+  const BigReal dielectric_1 = 1/Node::Object()->simParameters->dielectric;
 
   const LJTable* const ljTable = LJTable::Instance();
   const Molecule* const mol = Node::Object()->molecule;
-  const BigReal dielectric_1 = 1/Node::Object()->simParameters->dielectric;
 
-#ifdef EXCLUDE14
+M14
+(
   const BigReal scale14 = Node::Object()->simParameters->scale14;
-#endif
+)
 
-#ifdef SWITCHING
+SW
+(
   const Real switchOn = Node::Object()->simParameters->switchingDist;
   const BigReal switchOn2 = switchOn*switchOn;
   const BigReal c0 = 1/(cutoff2-switchOn2);
@@ -83,16 +94,7 @@
   const BigReal c3 = c1 * 4;
   const BigReal c5 = 1/cutoff2;
   const BigReal c6 = -4 * c5;
-  BigReal switchVal;                   //  Value of switching function
-  BigReal shiftVal;                    //  Value of shifting function
-  BigReal dShiftVal;                   //  d(shiftVal)/dr
-  BigReal dSwitchVal;                  //  d(switchVal)/dr
-#else
-#define switchVal 1
-#define shiftVal 1
-#define dShiftVal 0
-#define dSwitchVal 0
-#endif
+)
 
   for ( int i = I_LOWER; i < I_UPPER; ++i )
   {
@@ -101,42 +103,55 @@
 
     Force & f_i = f[I_SUB];
 
-    const BigReal kq_i = COLOUMB * a_i.charge * dielectric_1;
+    const BigReal M14( kq_i_u ) NOM14( kq_i ) =
+    			COLOUMB * a_i.charge * dielectric_1;
+
+    M14( const BigReal kq_i_s = kq_i_u * scale14; )
 
     for ( int j = J_LOWER; j < J_UPPER; ++j )
     {
       const Position & p_j = p[J_SUB];
 
-      f_vdw = p_i - p_j;
+      Vector f_vdw = p_i - p_j;
 
-      if ( ( r2 = f_vdw.length2() ) > cutoff2 ) continue;
+      const BigReal r2 = f_vdw.length2()
+
+      if ( r2 > cutoff2 ) continue;
+
+      M14 ( BigReal kq_i = kq_i_u; )
 
       const AtomProperties & a_j = a[J_SUB];
 
-      const LJTable::TableEntry * lj_pars = 
+      const LJTable::TableEntry * NOM14( const ) lj_pars = 
 		ljTable->table_val(a_i.type, a_j.type);
-      BigReal scaleFactor = 1.;
-
+		
       if ( r2 <= lj_pars->exclcut2 )
       {
 	if ( mol->checkexcl(a_i.id,a_j.id) ) continue;
 	
-#ifdef EXCLUDE14
+M14
+(
 	else if ( mol->check14excl(a_i.id,a_j.id) )
 	{
 	  lj_pars = ljTable->table_val(a_i.type, a_j.type, 1);
-	  scaleFactor = scale14;
+	  kq_i = kq_i_s;
 	}
-#endif
+)
 
       }
 
       Force & f_j = f[J_SUB];
 
-      r = sqrt(r2);
-      r_1 = 1/r;
+      const BigReal r = sqrt(r2);
+      const BigReal r_1 = 1/r;
 
-#ifdef SWITCHING
+SW
+(
+      BigReal switchVal;
+      BigReal shiftVal;
+      BigReal dSwitchVal;
+      BigReal dShiftVal;
+
       if (r > switchOn)
       {
 	const BigReal c2 = cutoff2-r2;
@@ -153,24 +168,23 @@
       shiftVal = 1 - r2*c5;
       dShiftVal = c6*shiftVal*r;
       shiftVal *= shiftVal;
-#endif
+)
 
-      const BigReal kqq = kq_i * a_j.charge * scaleFactor;
+      const BigReal kqq = kq_i * a_j.charge;
 
       BigReal f = kqq*r_1;
       
-      electEnergy += f*shiftVal;
+      electEnergy += f SW( * shiftVal );
       
-      f *= r_1*(shiftVal*r_1 - dShiftVal);
+      f *= r_1*( SW( shiftVal * ) r_1 SW( - dShiftVal ) );
 
-      f_elec = f_vdw*f;
+      const Vector f_elec = f_vdw*f;
 
       f_i += f_elec;
       f_j -= f_elec;
 
-      r_6 = r_1*r_1*r_1;
-      r_6 *= r_6;
-      r_12 = r_6*r_6;
+      BigReal r_6 = r_1*r_1*r_1; r_6 *= r_6;
+      const BigReal r_12 = r_6*r_6;
 
       const BigReal A = lj_pars->A;
       const BigReal B = lj_pars->B;
@@ -179,7 +193,8 @@
 
       vdwEnergy += switchVal*AmBterm;
       
-      f_vdw *= (switchVal* 6 * (A*r_12 + AmBterm)*r_1-AmBterm*dSwitchVal)*r_1;
+      f_vdw *= ( SW( switchVal * ) 6 * (A*r_12 + AmBterm) *
+			r_1 SW( - AmBterm*dSwitchVal ) )*r_1;
 
       f_i += f_vdw;
       f_j -= f_vdw;
@@ -192,7 +207,7 @@
  *
  *	$RCSfile: ComputeNonbondedHack.h,v $
  *	$Author: jim $	$Locker:  $		$State: Exp $
- *	$Revision: 1.1 $	$Date: 1996/11/06 21:17:11 $
+ *	$Revision: 1.2 $	$Date: 1996/11/06 23:03:44 $
  *
  ***************************************************************************
  * REVISION HISTORY:
