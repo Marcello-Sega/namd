228480c09597537bd9e632eabf57ec6c51265724
Neal Krawetz <nealk@ks.uiuc.edu>
1996-11-08 11:15:11 -0600

    Initial revision

---
+++ src/InfoStream.h
@@ -0,0 +1,58 @@
+/***************************************************************************/
+/*                                                                         */
+/*              (C) Copyright 1996 The Board of Trustees of the            */
+/*                          University of Illinois                         */
+/*                           All Rights Reserved                           */
+/*                                                                         */
+/***************************************************************************/
+
+/***************************************************************************
+ * DESCRIPTION:
+ *  Defines a new stream: iout, for "i"nforming consoles.
+ ***************************************************************************/
+#ifndef INFOSTREAM_H
+#define INFOSTREAM_H
+
+#include <iostream.h>	// for cout
+#include <strstream.h>	// for ostrstream
+#include "ckdefs.h"	// for CPrintf
+
+class infostream : public ostrstream
+  {
+  private:
+  char Buffer[1024];
+
+  public:
+  infostream() : ostrstream(Buffer,sizeof(Buffer)) {;}
+
+  /* output using CPrintf() (end by inform) */
+  void endi()
+    {
+    *this << ends;
+    CPrintf("%s",Buffer);
+    this->seekp(0);
+    return;
+    }
+
+  /* output to stdout (end by console) */
+  void endc()
+    {
+    *this << ends;
+    cout << Buffer;
+    this->seekp(0);
+    return;
+    }
+
+  };
+
+extern infostream iout;
+
+/** now we have a stream.  Let's tell the stream when/where to output **/
+/** iout << endi;  (send output to information console -- CPrintf) **/
+/** iout << endc;  (send output to host console -- cout) **/
+char * endi(infostream& s) { s.endi(); return ""; }
+char * endc(infostream& s) { s.endc(); return ""; }
+#define endi endi(iout)
+#define endc endc(iout)
+
+#endif
