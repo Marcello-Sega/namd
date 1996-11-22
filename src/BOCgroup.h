0d0dc21882105a238a96ae0034c829fb684246d8
Aritomo Shinozaki <ari@ks.uiuc.edu>
1996-11-21 18:18:51 -0600

    Initial revision

---
+++ src/BOCgroup.h
@@ -0,0 +1,33 @@
+/***************************************************************************/
+/*                                                                         */
+/*              (C) Copyright 1996 The Board of Trustees of the            */
+/*                          University of Illinois                         */
+/*                           All Rights Reserved                           */
+/*                                                                         */
+/***************************************************************************/
+
+/***************************************************************************
+ * DESCRIPTION:
+ *
+ ***************************************************************************/
+
+#ifndef BOCGROUP_H
+#define BOCGROUP_H
+
+#include "ckdefs.h"
+#include "chare.h"
+#include "c++interface.h"
+
+class BOCgroup {
+public:
+  int workDistrib;
+  int patchMgr;
+  int node;
+};
+
+class BOCclass : public groupmember {
+protected:
+    static BOCgroup group;
+};
+
+#endif /* BOCGROUP_H */
