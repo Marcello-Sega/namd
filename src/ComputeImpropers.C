997b85cf39a1f965ba699d9087c851cce818f415
Neal Krawetz <nealk@ks.uiuc.edu>
1996-11-26 10:33:35 -0600

    Initial revision

---
+++ src/ComputeImpropers.C
@@ -0,0 +1,233 @@
+/***************************************************************************/
+/*                                                                         */
+/*              (C) Copyright 1996 The Board of Trustees of the            */
+/*                          University of Illinois                         */
+/*                           All Rights Reserved                           */
+/*									   */
+/***************************************************************************/
+
+/***************************************************************************
+ * DESCRIPTION:
+ *
+ ***************************************************************************/
+
+#include "ComputeImpropers.h"
+#include "Molecule.h"
+#include "Parameters.h"
+#include "Node.h"
+
+
+#define DEBUGM
+#include "Debug.h"
+
+void ImproperElem::addTuplesForAtom
+  (void *voidlist, AtomID atomID, Molecule *molecule)
+{
+      DebugM(1, "::addTuplesForAtom - atomID " << atomID << endl );
+      UniqueSortedArray<ImproperElem> &improperList =
+                  *( (UniqueSortedArray<ImproperElem>*) voidlist );
+
+      DebugM(1, "::addTuplesForAtom - current list size " << improperList.size() << endl );
+
+      /* get list of all impropers for the atom */
+      LintList *impropers = molecule->get_impropers_for_atom(atomID);
+      DebugM(1, "::addTuplesForAtom - atomID " << atomID << endl );
+      DebugM(1, "::addTuplesForAtom - impropers->head()" << impropers->head() << endl );
+
+      /* cycle through each improper */
+      int improperNum = impropers->head();
+      while(improperNum != LIST_EMPTY)
+      {
+        /* store improper in the list */
+        DebugM(1, "::addTuplesForAtom - adding improper " << improperNum << endl );
+        improperList.add(ImproperElem(molecule->get_improper(improperNum)));
+        improperNum = impropers->next();
+      }
+}
+
+BigReal ImproperElem::computeForce(void)
+{
+  DebugM(3, "::computeForce() localIndex = " << localIndex[0] << " "
+               << localIndex[1] << " " << localIndex[2] << endl);
+
+  Vector r12, r23, r34;	// vector between atoms
+  Vector A,B,C;		// cross products
+  BigReal rA, rB, rC;	// length of vectors A, B, and C
+  BigReal energy=0;	// energy from the angle
+  BigReal phi;		// angle between the plans
+  BigReal cos_phi;	// cos(phi)
+  BigReal sin_phi;	// sin(phi)
+  Vector dcosdA;	// Derivative d(cos(phi))/dA
+  Vector dcosdB;	// Derivative d(cos(phi))/dB
+  Vector dsindC;	// Derivative d(sin(phi))/dC
+  Vector dsindB;	// Derivative d(sin(phi))/dB
+  BigReal K,K1;		// energy constants
+  BigReal diff;		// for periodicity
+  Force f1,f2,f3;	// force components
+  Real k, delta;	// angle information
+  int n;		// angle information
+
+  DebugM(3, "::computeForce() -- starting with improper type " << improperType << endl);
+
+  // get the improper information
+  int multiplicity = Node::Object()->parameters->get_improper_multiplicity(improperType);
+
+  //  Calculate the vectors between atoms
+  const Position & pos0 = p[0]->x[localIndex[0]];
+  const Position & pos1 = p[1]->x[localIndex[1]];
+  const Position & pos2 = p[2]->x[localIndex[2]];
+  const Position & pos3 = p[3]->x[localIndex[3]];
+  r12 = pos0-pos1;
+  r23 = pos1-pos2;
+  r34 = pos2-pos3;
+
+  //  Calculate the cross products
+  A = cross(r12,r23);
+  B = cross(r23,r34);
+  C = cross(r23,A);
+
+  //  Calculate the distances
+  rA = A.length();
+  rB = B.length();
+  rC = C.length();
+
+  //  Calculate the sin and cos
+  cos_phi = A*B/(rA*rB);
+  sin_phi = C*B/(rC*rB);
+
+  //  Normalize B
+  rB = 1.0/rB;
+  B *= rB;
+
+  phi= -atan2(sin_phi,cos_phi);
+  CHECK_DOMAIN();
+
+  if (fabs(sin_phi) > 0.1)
+  {
+    //  Normalize A
+    rA = 1.0/rA;
+    A *= rA;
+    dcosdA = rA*(cos_phi*A-B);
+    dcosdB = rB*(cos_phi*B-A);
+  }
+  else
+  {
+    //  Normalize C
+    rC = 1.0/rC;
+    C *= rC;
+    dsindC = rC*(sin_phi*C-B);
+    dsindB = rB*(sin_phi*B-C);
+  }
+
+  //  Loop through the multiple parameter sets for this
+  //  bond.  We will only loop more than once if this
+  //  has multiple parameter sets from Charmm22
+  for (int mult_num=0; mult_num<multiplicity; mult_num++)
+  {
+    /* get angle information */
+    Node::Object()->parameters->get_improper_params(&k,&n,&delta,improperType,mult_num);
+
+    //  Calculate the energy
+    if (n)
+    {
+      //  Periodicity is greater than 0, so use cos form
+      K = k*(1+cos(n*phi + delta));
+      K1 = -n*k*sin(n*phi + delta);
+    }
+    else
+    {
+      //  Periodicity is 0, so just use the harmonic form
+      diff = phi-delta;
+      if (diff < -PI)           diff += TWOPI;
+      else if (diff > PI)       diff -= TWOPI;
+
+      K = k*diff*diff;
+      K1 = 2.0*k*diff;
+    }
+
+    //  Add the energy from this improper to the total energy
+    energy += K;
+
+    //  Next, we want to calculate the forces.  In order
+    //  to do that, we first need to figure out whether the
+    //  sin or cos form will be more stable.  For this,
+    //  just look at the value of phi
+    if (fabs(sin_phi) > 0.1)
+    {
+      //  use the sin version to avoid 1/cos terms
+      K1 = K1/sin_phi;
+
+      #if 0
+      /** this is not a cross product...
+      "Explore" shows it as a very complex
+      function.  Double checked with docs and with Robert. -NK **/
+      f1 = cross(K1,r23,dcosdA);
+      f1.y = K1*(r23.z*dcosdA.x - r23.x*dcosdA.z);
+
+
+      f3 = cross(K1,dcosdB,r23);
+      f3.y = K1*(r23.x*dcosdB.z - r23.z*dcosdB.x);
+
+      f2 = K1*(cross(dcosdA,r12) + cross(r34,dcosdB));
+      f2.y = K1*(r12.x*dcosdA.z - r12.z*dcosdA.x
+               + r34.z*dcosdB.x - r34.x*dcosdB.z);
+      #else
+      f1.x = K1*(r23.y*dcosdA.z - r23.z*dcosdA.y);
+      f1.y = K1*(r23.z*dcosdA.x - r23.x*dcosdA.z);
+      f1.z = K1*(r23.x*dcosdA.y - r23.y*dcosdA.x);
+
+      f3.x = K1*(r23.z*dcosdB.y - r23.y*dcosdB.z);
+      f3.y = K1*(r23.x*dcosdB.z - r23.z*dcosdB.x);
+      f3.z = K1*(r23.y*dcosdB.x - r23.x*dcosdB.y);
+
+      f2.x = K1*(r12.z*dcosdA.y - r12.y*dcosdA.z
+               + r34.y*dcosdB.z - r34.z*dcosdB.y);
+      f2.y = K1*(r12.x*dcosdA.z - r12.z*dcosdA.x
+               + r34.z*dcosdB.x - r34.x*dcosdB.z);
+      f2.z = K1*(r12.y*dcosdA.x - r12.x*dcosdA.y
+             + r34.x*dcosdB.y - r34.y*dcosdB.x);
+      #endif
+    }
+    else
+    {
+      //  This angle is closer to 0 or 180 than it is to
+      //  90, so use the cos version to avoid 1/sin terms
+      K1 = -K1/cos_phi;
+
+      f1.x = K1*((r23.y*r23.y + r23.z*r23.z)*dsindC.x
+                - r23.x*r23.y*dsindC.y
+                - r23.x*r23.z*dsindC.z);
+      f1.y = K1*((r23.z*r23.z + r23.x*r23.x)*dsindC.y
+                - r23.y*r23.z*dsindC.z
+                - r23.y*r23.x*dsindC.x);
+      f1.z = K1*((r23.x*r23.x + r23.y*r23.y)*dsindC.z
+                - r23.z*r23.x*dsindC.x
+                - r23.z*r23.y*dsindC.y);
+
+      f3 = cross(K1,dsindB,r23);
+
+      f2.x = K1*(-(r23.y*r12.y + r23.z*r12.z)*dsindC.x
+             +(2.0*r23.x*r12.y - r12.x*r23.y)*dsindC.y
+             +(2.0*r23.x*r12.z - r12.x*r23.z)*dsindC.z
+             +dsindB.z*r34.y - dsindB.y*r34.z);
+      f2.y = K1*(-(r23.z*r12.z + r23.x*r12.x)*dsindC.y
+             +(2.0*r23.y*r12.z - r12.y*r23.z)*dsindC.z
+             +(2.0*r23.y*r12.x - r12.y*r23.x)*dsindC.x
+             +dsindB.x*r34.z - dsindB.z*r34.x);
+      f2.z = K1*(-(r23.x*r12.x + r23.y*r12.y)*dsindC.z
+             +(2.0*r23.z*r12.x - r12.z*r23.x)*dsindC.x
+             +(2.0*r23.z*r12.y - r12.z*r23.y)*dsindC.y
+             +dsindB.y*r34.x - dsindB.x*r34.y);
+    }
+  } /* for multiplicity */
+
+  /* store the forces */
+  p[0]->f[localIndex[0]] += f1;
+  p[1]->f[localIndex[1]] += f2 - f1;
+  p[2]->f[localIndex[2]] += f3 - f2;
+  p[3]->f[localIndex[3]] += -f3;
+
+  DebugM(3, "::computeForce() -- ending with delta energy " << energy << endl);
+  return(energy);
+}
+
---
+++ src/ComputeImpropers.h
@@ -0,0 +1,145 @@
+/***************************************************************************/
+/*                                                                         */
+/*              (C) Copyright 1996 The Board of Trustees of the            */
+/*                          University of Illinois                         */
+/*                           All Rights Reserved                           */
+/*									   */
+/***************************************************************************/
+
+/***************************************************************************
+ * DESCRIPTION:
+ *
+ ***************************************************************************/
+
+#ifndef COMPUTEIMPROPERS_H
+#define COMPUTEIMPROPERS_H
+
+#include "ComputeHomeTuples.h"
+class Molecule;
+
+class ImproperElem {
+public:
+    // ComputeHomeTuples interface
+    enum { size = 4 };
+    AtomID atomID[size];
+    int    localIndex[size];
+    TuplePatchElem *p[size];
+    BigReal computeForce(void);
+    // The following is evil, but the compiler chokes otherwise. (JCP)
+    static void addTuplesForAtom(void*, AtomID, Molecule*);
+
+    // Internal data
+    Index improperType;
+
+
+  ImproperElem() {
+	atomID[0] = -1;
+	atomID[1] = -1;
+	atomID[2] = -1;
+	atomID[3] = -1;
+	p[0] = NULL;
+	p[1] = NULL;
+	p[2] = NULL;
+	p[3] = NULL;
+  }
+  ImproperElem(const Improper *a) {
+    atomID[0] = a->atom1;
+    atomID[1] = a->atom2;
+    atomID[2] = a->atom3;
+    atomID[3] = a->atom4;
+    improperType = a->improper_type;
+  }
+
+  ImproperElem(AtomID atom0, AtomID atom1, AtomID atom2, AtomID atom3) {
+    if (atom0 > atom3) {  // Swap end atoms so lowest is first!
+      AtomID tmp = atom3; atom3 = atom0; atom0 = tmp; 
+      tmp = atom1; atom1 = atom2; atom2 = tmp;
+    }
+    atomID[0] = atom0;
+    atomID[1] = atom1;
+    atomID[2] = atom2;
+    atomID[3] = atom3;
+  }
+  ~ImproperElem() {};
+
+  int operator==(const ImproperElem &a) const {
+    return (a.atomID[0] == atomID[0] && a.atomID[1] == atomID[1] &&
+        a.atomID[2] == atomID[2] && a.atomID[3] == atomID[3]);
+  }
+
+  int operator<(const ImproperElem &a) const {
+    return  (atomID[0] < a.atomID[0] ||
+            (atomID[0] == a.atomID[0] &&
+            (atomID[1] < a.atomID[1] ||
+            (atomID[1] == a.atomID[1] &&
+            (atomID[2] < a.atomID[2] ||
+            (atomID[2] == a.atomID[2] &&
+             atomID[3] < a.atomID[3] 
+	     ))))));
+  }
+};
+
+class ComputeImpropers : public ComputeHomeTuples<ImproperElem>
+{
+public:
+
+  ComputeImpropers(ComputeID c) : ComputeHomeTuples<ImproperElem>(c) { ; }
+
+};
+
+#endif
+/***************************************************************************
+ * RCS INFORMATION:
+ *
+ *	$RCSfile: ComputeImpropers.h,v $
+ *	$Author: nealk $	$Locker:  $		$State: Exp $
+ *	$Revision: 1.1 $	$Date: 1996/11/26 16:33:35 $
+ *
+ ***************************************************************************
+ * REVISION HISTORY:
+ *
+ * $Log: ComputeImpropers.h,v $
+ * Revision 1.1  1996/11/26 16:33:35  nealk
+ * Initial revision
+ *
+ * Revision 1.7  1996/11/22 01:45:28  jim
+ * restructured, fixed bugs, now seems to work
+ *
+ * Revision 1.6  1996/11/19 06:58:37  jim
+ * first compiling templated version, needed ugly void* hack
+ *
+ * Revision 1.5  1996/11/18 21:28:48  ari
+ * *** empty log message ***
+ *
+ * Revision 1.4  1996/11/04 20:06:17  nealk
+ * Now it compiles :-)
+ *
+ * Revision 1.3  1996/11/04 19:29:02  nealk
+ * Added angleForce() to system, but it is untested.
+ *
+ * Revision 1.2  1996/11/04 16:55:46  ari
+ * *** empty log message ***
+ *
+ * Revision 1.1  1996/11/01 21:20:45  ari
+ * Initial revision
+ *
+ * Revision 1.3  1996/10/16 08:22:39  ari
+ * *** empty log message ***
+ *
+ * Revision 1.1  1996/08/19 22:07:49  ari
+ * Initial revision
+ *
+ * Revision 1.4  1996/07/16 01:54:12  ari
+ * *** empty log message ***
+ *
+ * Revision 1.3  96/07/16  01:10:26  01:10:26  ari (Aritomo Shinozaki)
+ * Fixed comments, added methods
+ * 
+ * Revision 1.2  1996/06/25 21:10:48  gursoy
+ * *** empty log message ***
+ *
+ * Revision 1.1  1996/06/24 14:12:26  gursoy
+ * Initial revision
+ *
+ ***************************************************************************/
+
