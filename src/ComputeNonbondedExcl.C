bc27bcbd47dbb42188c93c1d32084d64e99465d1
Neal Krawetz <nealk@ks.uiuc.edu>
1996-12-03 11:17:03 -0600

    Initial revision

---
+++ src/ComputeNonbondedExcl.C
@@ -0,0 +1,54 @@
+/***************************************************************************/
+/*                                                                         */
+/*              (C) Copyright 1996 The Board of Trustees of the            */
+/*                          University of Illinois                         */
+/*                           All Rights Reserved                           */
+/*									   */
+/***************************************************************************/
+
+/***************************************************************************
+ * DESCRIPTION:
+ *
+ ***************************************************************************/
+
+#include "ComputeNonbonedExcl.h"
+#include "Molecule.h"
+#include "Parameters.h"
+#include "Node.h"
+
+#include "Debug.h"
+
+void NonbondedExclElem::addTuplesForAtom
+  (void *voidlist, AtomID atomID, Molecule *molecule)
+{
+      DebugM(1, "::addTuplesForAtom - atomID " << atomID << endl );
+      UniqueSortedArray<NonbondedExclElem> &nonbondedexclList =
+                  *( (UniqueSortedArray<NonbondedExclElem>*) voidlist );
+
+      DebugM(1, "::addTuplesForAtom - current list size " << nonbondedexclList.size() << endl );
+
+      /* get list of all nonbondedexcls for the atom */
+      LintList *nonbondedexcls = molecule->get_nonbondedexcls_for_atom(atomID);
+      DebugM(1, "::addTuplesForAtom - atomID " << atomID << endl );
+      DebugM(1, "::addTuplesForAtom - nonbondedexcls->head()" << nonbondedexcls->head() << endl );
+
+      /* cycle through each nonbondedexcl */
+      int nonbondedexclNum = nonbondedexcls->head();
+      while(nonbondedexclNum != LIST_EMPTY)
+      {
+        /* store nonbondedexcl in the list */
+        DebugM(1, "::addTuplesForAtom - adding nonbondedexcl " << nonbondedexclNum << endl );
+        nonbondedexclList.add(NonbondedExclElem(molecule->get_nonbondedexcl(nonbondedexclNum)));
+        nonbondedexclNum = nonbondedexcls->next();
+      }
+}
+
+BigReal NonbondedExclElem::computeForce(void)
+{
+  DebugM(1, "::computeForce() localIndex = " << localIndex[0] << " "
+               << localIndex[1] << endl);
+
+  DebugM(3, "::computeForce() -- ending with delta energy " << energy << endl);
+  return(energy);
+}
+
---
+++ src/ComputeNonbondedExcl.h
@@ -0,0 +1,98 @@
+/***************************************************************************/
+/*                                                                         */
+/*              (C) Copyright 1996 The Board of Trustees of the            */
+/*                          University of Illinois                         */
+/*                           All Rights Reserved                           */
+/*									   */
+/***************************************************************************/
+
+/***************************************************************************
+ * DESCRIPTION:
+ *
+ ***************************************************************************/
+
+#ifndef COMPUTENONBONDEDEXCL_H
+#define COMPUTENONBONDEDEXCL_H
+
+#include "ComputeHomeTuples.h"
+class Molecule;
+
+class NonbondedExclElem {
+public:
+    // ComputeHomeTuples interface
+    enum { size = 2 };
+    AtomID atomID[size];
+    int    localIndex[size];
+    TuplePatchElem *p[size];
+    BigReal computeForce(void);
+    // The following is evil, but the compiler chokes otherwise. (JCP)
+    static void addTuplesForAtom(void*, AtomID, Molecule*);
+
+    // Internal data
+    Index nonbondedexclType;
+
+
+  NonbondedExclElem() {
+    atomID[0] = -1;
+    atomID[1] = -1;
+    p[0] = NULL;
+    p[1] = NULL;
+  }
+  NonbondedExclElem(const NonbondedExcl *a) {
+    atomID[0] = a->atom1;
+    atomID[1] = a->atom2;
+    nonbondedexclType = a->nonbondedexcl_type;
+  }
+
+  NonbondedExclElem(AtomID atom0, AtomID atom1) {
+    if (atom0 > atom1) {  // Swap end atoms so lowest is first!
+      AtomID tmp = atom1; atom1 = atom0; atom0 = tmp; 
+    }
+    atomID[0] = atom0;
+    atomID[1] = atom1;
+  }
+  ~NonbondedExclElem() {};
+
+  int operator==(const NonbondedExclElem &a) const {
+    return (a.atomID[0] == atomID[0] && a.atomID[1] == atomID[1]);
+  }
+
+  int operator<(const NonbondedExclElem &a) const {
+    return (atomID[0] < a.atomID[0] ||
+            (atomID[0] == a.atomID[0] &&
+            (atomID[1] < a.atomID[1]) ));
+  }
+};
+
+class ComputeNonbondedExcls : public ComputeHomeTuples<NonbondedExclElem>
+{
+public:
+
+  ComputeNonbondedExcls(ComputeID c) : ComputeHomeTuples<NonbondedExclElem>(c) { ; }
+
+};
+
+#endif
+/***************************************************************************
+ * RCS INFORMATION:
+ *
+ *	$RCSfile: ComputeNonbondedExcl.h,v $
+ *	$Author: nealk $	$Locker:  $		$State: Exp $
+ *	$Revision: 1.1 $	$Date: 1996/12/03 17:17:03 $
+ *
+ ***************************************************************************
+ * REVISION HISTORY:
+ *
+ * $Log: ComputeNonbondedExcl.h,v $
+ * Revision 1.1  1996/12/03 17:17:03  nealk
+ * Initial revision
+ *
+ * Revision 1.2  1996/12/03 15:15:40  nealk
+ * Removed tons-o-debugging.
+ *
+ * Revision 1.1  1996/12/03 14:53:42  nealk
+ * Initial revision
+ *
+ *
+ ***************************************************************************/
+
