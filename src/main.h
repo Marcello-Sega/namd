0cd56ee9bed420251aea55732054915b5f1cf565
Jim Phillips <jim@ks.uiuc.edu>
1996-12-05 11:56:58 -0600

    added pack and unpack functions for proxy messages

--- src/main.h
+++ src/main.h
@@ -59,18 +59,78 @@ class ProxyAtomsMsg : public comm_object {
 public:
   PatchID patch;
   AtomIDList atomIDList;
+  void * pack (int *length)
+  {
+    int size = atomIDList.size();
+    char *buffer = (char*)new_packbuffer(this,
+    	sizeof(int) + size * sizeof(AtomID));
+    *((int*)buffer) = size;
+    AtomID *data = (AtomID*)(buffer+sizeof(int));
+    for ( int i = 0; i < size; ++i )
+      data[i] = atomIDList[i];
+    return buffer;
+  }
+  void unpack (void *in)
+  {
+    char *buffer = (char*)in;
+    int size = *((int*)buffer);
+    atomIDList.resize(size);
+    AtomID *data = (AtomID*)(buffer+sizeof(int));
+    for ( int i = 0; i < size; ++i )
+      atomIDList[i] = buffer[i];
+  }
 };
 
 class ProxyDataMsg : public comm_object {
 public:
   PatchID patch;
   PositionList positionList;
+  void * pack (int *length)
+  {
+    int size = positionList.size();
+    char *buffer = (char*)new_packbuffer(this,
+    	sizeof(int) + size * sizeof(Position));
+    *((int*)buffer) = size;
+    Position *data = (Position*)(buffer+sizeof(int));
+    for ( int i = 0; i < size; ++i )
+      data[i] = positionList[i];
+    return buffer;
+  }
+  void unpack (void *in)
+  {
+    char *buffer = (char*)in;
+    int size = *((int*)buffer);
+    positionList.resize(size);
+    Position *data = (Position*)(buffer+sizeof(int));
+    for ( int i = 0; i < size; ++i )
+      positionList[i] = buffer[i];
+  }
 };
 
 class ProxyResultMsg : public comm_object {
 public:
   PatchID patch;
   ForceList forceList;
+  void * pack (int *length)
+  {
+    int size = forceList.size();
+    char *buffer = (char*)new_packbuffer(this,
+    	sizeof(int) + size * sizeof(Force));
+    *((int*)buffer) = size;
+    Force *data = (Force*)(buffer+sizeof(int));
+    for ( int i = 0; i < size; ++i )
+      data[i] = forceList[i];
+    return buffer;
+  }
+  void unpack (void *in)
+  {
+    char *buffer = (char*)in;
+    int size = *((int*)buffer);
+    forceList.resize(size);
+    Force *data = (Force*)(buffer+sizeof(int));
+    for ( int i = 0; i < size; ++i )
+      forceList[i] = buffer[i];
+  }
 };
 
 class Compute;
@@ -88,12 +148,15 @@ public:
  *
  *	$RCSfile: main.h,v $
  *	$Author: jim $	$Locker:  $		$State: Exp $
- *	$Revision: 1.11 $	$Date: 1996/12/05 17:15:23 $
+ *	$Revision: 1.12 $	$Date: 1996/12/05 17:56:58 $
  *
  ***************************************************************************
  * REVISION HISTORY:
  *
  * $Log: main.h,v $
+ * Revision 1.12  1996/12/05 17:56:58  jim
+ * added pack and unpack functions for proxy messages
+ *
  * Revision 1.11  1996/12/05 17:15:23  jim
  * added proxy messages
  *
