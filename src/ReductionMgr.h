3a81667b69eee5fe35e439b036b072c012ef22b5
Jim Phillips <jim@ks.uiuc.edu>
1996-12-10 18:08:03 -0600

    split into two classes

--- src/ReductionMgr.h
+++ src/ReductionMgr.h
@@ -7,21 +7,16 @@
 
 typedef enum
 {
-  SUBMIT_KINETIC_ENERGY,
-  SUBMIT_BOND_ENERGY,
-  SUBMIT_ANGLE_ENERGY,
-  SUBMIT_DIHEDRAL_ENERGY,
-  SUBMIT_IMPROPER_ENERGY,
-  SUBMIT_ELECT_ENERGY,
-  SUBMIT_LJ_ENERGY,
-  SUBMIT_LONG_RANGE_ENERGY,
-  SUBMIT_MAX_RESERVED
-} SubmitTag;
-
-typedef enum
-{
-  REQUIRE_MAX_RESERVED
-} RequireTag;
+  REDUCTION_KINETIC_ENERGY,
+  REDUCTION_BOND_ENERGY,
+  REDUCTION_ANGLE_ENERGY,
+  REDUCTION_DIHEDRAL_ENERGY,
+  REDUCTION_IMPROPER_ENERGY,
+  REDUCTION_ELECT_ENERGY,
+  REDUCTION_LJ_ENERGY,
+  REDUCTION_LONG_RANGE_ENERGY,
+  REDUCTION_MAX_RESERVED
+} ReductionTag;
 
 class ReductionMgr
 {
@@ -33,48 +28,31 @@ public:
   // and the first eight after a tensor to allow storage.  -JCP
 
   // (un)register to submit data for reduction
-  // raises an error if reductions or broadcasts are active
-  register(SubmitTag tag);
-  unregister(SubmitTag tag);
+  // may cause an error if reductions are active
+  register(ReductionTag tag);
+  unregister(ReductionTag tag);
 
   // submit data for reduction
   // more == 1 signals immediate submission of other data
-  submit(int seq, SubmitTag tag, BigReal data, int more=0);
-  // submit(int seq, SubmitTag tag, Vector data, int more=0);
-  // submit(int seq, SubmitTag tag, Tensor data, int more=0);
+  submit(int seq, ReductionTag tag, BigReal data, int more=0);
+  submit(int seq, ReductionTag tag, Vector data, int more=0);
+  submit(int seq, ReductionTag tag, Tensor data, int more=0);
 
   // pass on submitting data
-  submit(int seq, SubmitTag tag, int more=0);
-
-  // (un)subscribe to broadcast data
-  // raises an error if reductions or broadcasts are active
-  subscribe(RequireTag tag);
-  unsubscribe(RequireTag tag);
-
-  // suspend until this data is ready
-  // should be called only from Sequencer thread
-  require(int seq, RequireTag tag, BigReal &data);
-  // require(int seq, RequireTag tag, Vector &data);
-  // require(int seq, RequireTag tag, Tensor &data);
-
-  // pass on requiring data
-  require(int seq, RequireTag tag);
+  submit(int seq, ReductionTag tag, int more=0);
 
   // methods for use by global sequencer
 
   // raises an error if reductions or broadcasts are active
-  request(SubmitTag tag);
-  unrequest(SubmitTag tag);
+  subscribe(ReductionTag tag);
+  unsubscribe(ReductionTag tag);
 
-  reduce(int seq, SubmitTag tag, BigReal &data);
-  // reduce(int seq, SubmitTag tag, Vector &data);
-  // reduce(int seq, SubmitTag tag, Tensor &data);
-
-  reduce(int seq, SubmitTag tag);
-
-  broadcast(int seq, RequireTag tag, BigReal data, int more=0);
-  // broadcast(int seq, RequireTag tag, Vector data, int more=0);
-  // broadcast(int seq, RequireTag tag, Tensor data, int more=0);
+  // suspend until this data is ready
+  // should be called only from Sequencer thread
+  require(int seq, ReductionTag tag, BigReal &data);
+  require(int seq, ReductionTag tag, Vector &data);
+  require(int seq, ReductionTag tag, Tensor &data);
+  require(int seq, ReductionTag tag); // pass on requiring data
 
 private:
 
