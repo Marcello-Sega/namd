bd3d0bbf086b7a32566c9ac07648dd133659051c
Neal Krawetz <nealk@ks.uiuc.edu>
1996-12-18 15:41:41 -0600

    Now uses iPE from InfoStream.h

--- src/Debug.h
+++ src/Debug.h
@@ -48,15 +48,11 @@
  *****************************************************************/
  #ifdef DEBUGM
 
-#include "ckdefs.h"
-#include "chare.h"
-#include "c++interface.h"
-
   #define Debug(x) (x)
   #define DebugM(level,format) \
 	{ \
 	  infostream Dout; \
-	  Dout << iFILE << "Pe(" << CMyPe() << ") "; \
+	  Dout << iFILE << iPE << ' '; \
 	  if ((level >= MIN_DEBUG_LEVEL) && (level <= MAX_DEBUG_LEVEL)) \
 	  { \
 	    if (level >= STDERR_LEVEL)	Dout << "ERROR (" << level << "): "; \
