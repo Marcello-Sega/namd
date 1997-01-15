ae6138f3fc231e5e0f4d6be5882bb7456c2ca9f1
Aritomo Shinozaki <ari@ks.uiuc.edu>
1997-01-15 11:09:43 -0600

    Initial revision

---
+++ src/Migration.h
@@ -0,0 +1,25 @@
+
+#ifndef MIGRATION_H
+#define MIGRATION_H
+
+#include "NamdTypes.h"
+
+struct MigrationElem {
+  AtomID atomID;
+  AtomProperties atomProp;
+  Position posInit, pos;
+  Velocity vel;
+  Force force, forceShort, forceLong;
+  int xdev, ydev, zdev;
+  MigrationElem(AtomID aid, AtomProperties ap, Position pInit,
+		Position p, Velocity v, 
+		Force f, Force f_short, Force f_long, 
+		int xd, int yd, int zd) : 
+      atomID(aid), atomProp(ap), posInit(pInit), pos(p), 
+      vel(v), force(f), forceShort(f_short), forceLong(f_long),
+      xdev(xd), ydev(yd), zdev(zd) {};
+}
+  
+typedef ResizeArray<MigrationElem> MigrationList;
+
+#endif // MIGRATION_H
