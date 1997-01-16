76c63980e9feca612af6d40a678524fd77cbc569
Jim Phillips <jim@ks.uiuc.edu>
1997-01-16 16:15:39 -0600

    Now prints out all energies, in a "nice" form (imho).

--- src/Controller.C
+++ src/Controller.C
@@ -25,11 +25,23 @@ Controller::Controller(NamdState *s) :
 	simParams(Node::Object()->simParameters),
 	reduction(ReductionMgr::Object())
 {
+    reduction->subscribe(REDUCTION_BOND_ENERGY);
+    reduction->subscribe(REDUCTION_ANGLE_ENERGY);
+    reduction->subscribe(REDUCTION_DIHEDRAL_ENERGY);
+    reduction->subscribe(REDUCTION_IMPROPER_ENERGY);
+    reduction->subscribe(REDUCTION_ELECT_ENERGY);
+    reduction->subscribe(REDUCTION_LJ_ENERGY);
     reduction->subscribe(REDUCTION_KINETIC_ENERGY);
 }
 
 Controller::~Controller(void)
 {
+    reduction->unsubscribe(REDUCTION_BOND_ENERGY);
+    reduction->unsubscribe(REDUCTION_ANGLE_ENERGY);
+    reduction->unsubscribe(REDUCTION_DIHEDRAL_ENERGY);
+    reduction->unsubscribe(REDUCTION_IMPROPER_ENERGY);
+    reduction->unsubscribe(REDUCTION_ELECT_ENERGY);
+    reduction->unsubscribe(REDUCTION_LJ_ENERGY);
     reduction->unsubscribe(REDUCTION_KINETIC_ENERGY);
 }
 
@@ -56,17 +68,56 @@ void Controller::algorithm(void)
     const BigReal timestep = simParams->dt;
     int step, cycle;
     int seq = 0;
-    BigReal ke;
-    reduction->require(seq, REDUCTION_KINETIC_ENERGY, ke);
-    DebugM(5, "Step: " << seq << " KE: " << ke << "\n");
+    BigReal bondEnergy;
+    BigReal angleEnergy;
+    BigReal dihedralEnergy;
+    BigReal improperEnergy;
+    BigReal electEnergy;
+    BigReal ljEnergy;
+    BigReal kineticEnergy;
+    BigReal totalEnergy;
+    reduction->require(seq, REDUCTION_BOND_ENERGY, bondEnergy);
+    reduction->require(seq, REDUCTION_ANGLE_ENERGY, angleEnergy);
+    reduction->require(seq, REDUCTION_DIHEDRAL_ENERGY, dihedralEnergy);
+    reduction->require(seq, REDUCTION_IMPROPER_ENERGY, improperEnergy);
+    reduction->require(seq, REDUCTION_ELECT_ENERGY, electEnergy);
+    reduction->require(seq, REDUCTION_LJ_ENERGY, ljEnergy);
+    reduction->require(seq, REDUCTION_KINETIC_ENERGY, kineticEnergy);
+    totalEnergy = bondEnergy + angleEnergy + dihedralEnergy + improperEnergy +
+	 electEnergy + ljEnergy + kineticEnergy;
+    iout << "ENERGY[" << seq << "] = { " <<
+	"bond: " << bondEnergy << ", " << 
+	"angle: " << angleEnergy << ", " << 
+	"dihedral: " << dihedralEnergy << ", " << 
+	"improper: " << improperEnergy << ", " << 
+	"elect: " << electEnergy << ", " << 
+	"lj: " << ljEnergy << ", " << 
+	"kinetic: " << kineticEnergy << ", " << 
+	"total: " << totalEnergy << " }\n" << endi;
     ++seq;
     for ( cycle = 0; cycle < numberOfCycles; ++cycle )
     {
         for ( step = 0; step < stepsPerCycle; ++step )
         {
-	    reduction->require(seq, REDUCTION_KINETIC_ENERGY, ke);
-	    DebugM(5, "Step: " << seq << " KE: " << ke << "\n");
-	    ++seq;
+    reduction->require(seq, REDUCTION_BOND_ENERGY, bondEnergy);
+    reduction->require(seq, REDUCTION_ANGLE_ENERGY, angleEnergy);
+    reduction->require(seq, REDUCTION_DIHEDRAL_ENERGY, dihedralEnergy);
+    reduction->require(seq, REDUCTION_IMPROPER_ENERGY, improperEnergy);
+    reduction->require(seq, REDUCTION_ELECT_ENERGY, electEnergy);
+    reduction->require(seq, REDUCTION_LJ_ENERGY, ljEnergy);
+    reduction->require(seq, REDUCTION_KINETIC_ENERGY, kineticEnergy);
+    totalEnergy = bondEnergy + angleEnergy + dihedralEnergy + improperEnergy +
+	 electEnergy + ljEnergy + kineticEnergy;
+    iout << "ENERGY[" << seq << "] = { " <<
+	"bond: " << bondEnergy << ", " << 
+	"angle: " << angleEnergy << ", " << 
+	"dihedral: " << dihedralEnergy << ", " << 
+	"improper: " << improperEnergy << ", " << 
+	"elect: " << electEnergy << ", " << 
+	"lj: " << ljEnergy << ", " << 
+	"kinetic: " << kineticEnergy << ", " << 
+	"total: " << totalEnergy << " }\n" << endi;
+    ++seq;
         }
     }
     DebugM(4, "Controller: Exiting.\n");
