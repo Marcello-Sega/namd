0e8ea96ef990dc54115ff07c7b18a4e104c608bc
Aritomo Shinozaki <ari@ks.uiuc.edu>
1997-01-17 13:24:44 -0600

    Minor changes to Migration.h - adding src,dst PatchID to MigrationElem

--- src/Migration.h
+++ src/Migration.h
@@ -5,17 +5,18 @@
 #include "NamdTypes.h"
 
 struct MigrationElem {
+  PatchID src, dest;
   AtomID atomID;
   AtomProperties atomProp;
   Position posInit, pos;
   Velocity vel;
   Force force, forceShort, forceLong;
   int xdev, ydev, zdev;
-  MigrationElem(AtomID aid, AtomProperties ap, Position pInit,
+  MigrationElem(PatchID s, AtomID aid, AtomProperties ap, Position pInit,
 		Position p, Velocity v, 
 		Force f, Force f_short, Force f_long, 
 		int xd, int yd, int zd) : 
-      atomID(aid), atomProp(ap), posInit(pInit), pos(p), 
+      src(s), atomID(aid), atomProp(ap), posInit(pInit), pos(p), 
       vel(v), force(f), forceShort(f_short), forceLong(f_long),
       xdev(xd), ydev(yd), zdev(zd) {};
 }
