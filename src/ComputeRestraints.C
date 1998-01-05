37e0f4570aded24fab0c9c7d7ec399712e65c052
Sergei Izrailev <sergei@ks.uiuc.edu>
1998-01-05 14:19:04 -0600

    removed cycling over an array of constrained atoms for moving restraints. Now it's just clean one-atom case.

--- src/ComputeRestraints.C
+++ src/ComputeRestraints.C
@@ -28,11 +28,14 @@ ComputeRestraints::ComputeRestraints(ComputeID c, PatchID pid)
 
 	//  Get parameters from the SimParameters object
 	consExp = simParams->constraintExp;
+	
+	//****** BEGIN moving constraints changes 
 	consMoveOn = simParams->movingConstraintsOn;
 	if (consMoveOn) {
 	  moveVel = simParams->movingConsVel;
 	  moveAtom = simParams->movingConsAtom;
 	}
+	//****** END moving constraints changes 
 
 }
 /*			END OF FUNCTION ComputeRestraints		*/
@@ -74,59 +77,10 @@ void ComputeRestraints::doForce(Position* p, Results* res, AtomProperties* a)
 	BigReal energy = 0;
 
 	// BEGIN moving constraint changes ****************************
-	
-	// !!!!!!!!! most of the following is a hack, which requires
-	// !!!!!!!!! recompilation if the velocity and/or atom numbers
-	// !!!!!!!!! change. The slight improvement is with the
-	// !!!!!!!!! introduction of moving restraints in the config file,
-	// !!!!!!!!! but for now it is only one atom that can be 
-	// !!!!!!!!! specified there. In general, it should be a list of
-	// !!!!!!!!! atoms.
-	// !!!!!!!!! I save the hack just for the heck of it.
-	// !!!!!!!!!                8/14/97              Sergei Izrailev
 
-	// The following specifies the velocity of the change of
-	// the constraint reference position. For now, it is the
-	// same velocity for all reference positions which we want to move.
-	// The indices of the atoms for which the reference positions 
-	// should be specified here too. 
+	// This version only allows one atom to be moved
 
-	int j; // loop counter
-	int flag; // temporary variable
-	int timestep;
-
-	timestep = patch->flags.seq;
-
-	// put the necessary numbers for the velocity (\AA / timestep) in here
-
-	// The numbers  are for pulling
-	// lipid 69 N50 in the pla2 tight structure towards LAM61N50 after
-	// 50 ps equilibration in the T-bath + 50 ps free dynamics. 
-	// The total displacement should be 
-	// 4 \AA in 100 ps.
-	//	Vector vel(-1.002556e-05, -3.636444e-06, -3.855208e-05);
-
-	// pulling of  lipid 51 in the loosely coupled pla2-dlpe
-	//	Vector vel(1.1938e-05, -5.328e-06, 3.498e-06);
-	
-	// list the (global) indices of the constrained atoms for which the 
-	// reference positions should be moved here, as well as number of 
-	// these atoms. NOTE: if the numbers are inconsistent, namd will
-	// crash. Atom N in the PDB file corresponds to index N-1 in namd.
-	int numToMove = 0; // 0 for equilibration
-	
-	// the change to take care of input from config file
-	if (consMoveOn) { // add 1 to include the atom specified in config file
-	  numToMove++;
-	}
-
-	int *toMove = new int[numToMove+1]; 
-	toMove[0] = moveAtom;  // atom index from the config file
-	//   toMove[0] = 3244;  // pla2 loose 
-	//   toMove[0] = 4042;  // tight 69_61
-	//   toMove[1] = 34;
-	//   toMove[2] = 56;
-	//   toMove[3] = 67;	
+	int currentTime = patch->flags.seq;
 
 	// END moving constraint changes ****************************
 
@@ -139,20 +93,18 @@ void ComputeRestraints::doForce(Position* p, Results* res, AtomProperties* a)
 
 	    // BEGIN moving constraint changes ****************************
 	    
-	    flag = 0;
-	    for ( j=0; j<numToMove; j++) {
-	      if (a[localID].id == toMove[j]) {
-		flag =1;
-		break;
+	    if (consMoveOn) {
+	      if (a[localID].id == moveAtom) { 
+
+		Rij = refPos + currentTime * moveVel  - p[localID];
+	      }
+	      else { // the default case
+		Rij = refPos - p[localID];
 	      }
 	    }
-
-	    if (flag) { // this reference position is to be moved
-	      Rij = refPos + timestep*moveVel - p[localID];
-	    }
-	    else { // the default case
-	      Rij = refPos - p[localID];
-	    }
+	    else {
+	     Rij = refPos - p[localID];
+	    } 
 
 	    // END moving constraint changes ****************************
 	    
@@ -201,12 +153,16 @@ void ComputeRestraints::doForce(Position* p, Results* res, AtomProperties* a)
  *
  *	$RCSfile: ComputeRestraints.C,v $
  *	$Author: sergei $	$Locker:  $		$State: Exp $
- *	$Revision: 1.2 $	$Date: 1997/08/18 20:16:04 $
+ *	$Revision: 1.3 $	$Date: 1998/01/05 20:19:04 $
  *
  ***************************************************************************
  * REVISION HISTORY:
  *
  * $Log: ComputeRestraints.C,v $
+ * Revision 1.3  1998/01/05 20:19:04  sergei
+ * removed cycling over an array of constrained atoms for moving
+ * restraints. Now it's just clean one-atom case.
+ *
  * Revision 1.2  1997/08/18 20:16:04  sergei
  * added moving restraint capability with input from config file
  * one atom only
@@ -216,3 +172,4 @@ void ComputeRestraints::doForce(Position* p, Results* res, AtomProperties* a)
  *
  *
  ***************************************************************************/
+
