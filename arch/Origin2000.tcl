d13993a639387f952ee815a021a7f1bfe7048d60
Jim Phillips <jim@ks.uiuc.edu>
2000-02-17 11:36:53 -0600

    Should find Tcl in /Projects/namd2 or $(HOME) now.

--- arch/Origin2000.tcl
+++ arch/Origin2000.tcl
@@ -1,7 +1,7 @@
 
-TCLDIR=$(HOME)/tcl
-TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.1
+TCLDIR=/Projects/namd2/tcl/origin
+TCLINCL=-I$(TCLDIR)/include -I$(HOME)/tcl/include
+TCLLIB=-L$(TCLDIR)/lib -L$(HOME)/tcl/lib -ltcl8.1
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
 
