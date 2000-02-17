63daf30c13bbf1b033a7597c7c64f303ae8b0cef
Jim Phillips <jim@ks.uiuc.edu>
2000-02-17 11:36:07 -0600

    Eliminated -ldl flag (not needed with proper library).

--- arch/Linux-i686.tcl
+++ arch/Linux-i686.tcl
@@ -1,7 +1,7 @@
 
 TCLDIR=/Projects/namd2/tcl/linux
 TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.1 -ldl
+TCLLIB=-L$(TCLDIR)/lib -ltcl8.1
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
 
