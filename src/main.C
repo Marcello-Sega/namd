d5462f5c5823d0448ab4d025b110419c9478e200
Jim Phillips <jim@ks.uiuc.edu>
2000-03-07 10:37:57 -0600

    Changed order of includes for T3E.

--- src/main.C
+++ src/main.C
@@ -19,10 +19,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h>
-#include <fcntl.h>              
 #include <sys/types.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
 #include <sys/socket.h>
 
 
