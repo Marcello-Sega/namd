204fc352bb4cbe10ea11b5c12afcf0d9714328c8
Jim Phillips <jim@ks.uiuc.edu>
2000-04-11 14:49:30 -0500

    Reorganization for optimizations.

--- src/ComputeAngles.C
+++ src/ComputeAngles.C
@@ -64,80 +64,59 @@ void AngleElem::computeForce(BigReal *reduction)
                << localIndex[1] << " " << localIndex[2] << endl);
 
   const Position & pos1 = p[0]->x[localIndex[0]].position;
-  const Position & pos2 = p[1]->x[localIndex[1]].position;
-  const Position & pos3 = p[2]->x[localIndex[2]].position;
-  Force force1(0,0,0);
-  Force force2(0,0,0);
-  Force force3(0,0,0);
   const Lattice & lattice = p[0]->p->lattice;
-
-  BigReal d12, d32, d13;	// distances between atoms
-  BigReal theta;	// theta
-  BigReal cos_theta;	// cos(theta)
-  BigReal sin_theta;	// sin(theta)
-  register BigReal diff;		// difference between theta and theta0
-  BigReal c1,c2;	// constant factors involved in force
-  BigReal energy;	// energy from the angle
-
-  DebugM(3, "::computeForce() -- starting with angle type " << angleType << endl);
-
-  // get the angle information
-  Real k = value->k;
-  Real theta0 = value->theta0;
-  Real k_ub = value->k_ub;
-  Real r_ub = value->r_ub;
-
-  // compute vectors between atoms and their distances
+  const Position & pos2 = p[1]->x[localIndex[1]].position;
   const Vector r12 = lattice.delta(pos1,pos2);
+  BigReal d12 = r12.length();
+  const Position & pos3 = p[2]->x[localIndex[2]].position;
   const Vector r32 = lattice.delta(pos3,pos2);
+  BigReal d32 = r32.length();
 
-  d12 = r12.length();
-  d32 = r32.length();
-
-  DebugM(3, "::computeForce() d12 = " << d12 << " d32 = " << d32 << endl);
-
+  BigReal cos_theta = (r12*r32)/(d12*d32);
+  //  This code is useless because below we divide by sin_theta!  -JCP
   //  Make sure that the cosine value is acceptable.  With roundoff, you
   //  can get values like 1.0+2e-16, which makes acos puke.  So instead,
   //  just set these kinds of values to exactly 1.0
-  cos_theta = (r12*r32)/(d12*d32);
-  if (cos_theta > 1.0) cos_theta = 1.0;
-  else if (cos_theta < -1.0) cos_theta = -1.0;
-  sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+  // if (cos_theta > 1.0) cos_theta = 1.0;
+  // else if (cos_theta < -1.0) cos_theta = -1.0;
+
+  BigReal k = value->k;
+  BigReal theta0 = value->theta0;
 
   //  Get theta
-  theta = acos(cos_theta);
+  BigReal theta = acos(cos_theta);
 
   //  Compare it to the rest angle
-  diff = theta - theta0;
-
-  DebugM(3, "::computeForce() theta = " << theta << " theta0 = " << theta0 << endl);
+  BigReal diff = theta - theta0;
 
   //  Add the energy from this angle to the total energy
-  energy = k *diff*diff;
+  BigReal energy = k *diff*diff;
 
   //  Normalize vector r12 and r32
   BigReal d12inv = 1. / d12;
   BigReal d32inv = 1. / d32;
 
   //  Calculate constant factor 2k(theta-theta0)/sin(theta)
+  BigReal sin_theta = sqrt(1.0 - cos_theta*cos_theta);
   diff *= (-2.0* k) / sin_theta;
-  c1 = diff * d12inv;
-  c2 = diff * d32inv;
+  BigReal c1 = diff * d12inv;
+  BigReal c2 = diff * d32inv;
 
   //  Calculate the actual forces
-  Force f12 = c1*(r12*(d12inv*cos_theta) - r32*d32inv);
-  force1 += f12; force2 -= f12;
-  Force f32 = c2*(r32*(d32inv*cos_theta) - r12*d12inv);
-  force3 += f32; force2 -= f32;
+  Force force1 = c1*(r12*(d12inv*cos_theta) - r32*d32inv);
+  Force force2 = force1;
+  Force force3 = c2*(r32*(d32inv*cos_theta) - r12*d12inv);
+  force2 += force3;  force2 *= -1;
 
   //  Check to see if we need to do the Urey-Bradley term
-  //  Forces are used only when atom1 or atom3 are local
-  if (k_ub > 0.0)
+  if (value->k_ub)
   {
 	//  Non-zero k_ub value, so calculate the harmonic
 	//  potential between the 1-3 atoms
+	BigReal k_ub = value->k_ub;
+	BigReal r_ub = value->r_ub;
 	Vector r13 = r12 - r32;
-	d13 = r13.length();
+	BigReal d13 = r13.length();
 	diff = d13- r_ub;
 
 	energy += k_ub *diff*diff;
--- src/ComputeDihedrals.C
+++ src/ComputeDihedrals.C
@@ -56,72 +56,35 @@ void DihedralElem::computeForce(BigReal *reduction)
   DebugM(3, "::computeForce() localIndex = " << localIndex[0] << " "
                << localIndex[1] << " " << localIndex[2] << endl);
 
-  // Vector r12, r23, r34;	// vector between atoms
-  Vector A,B,C;		// cross products
-  BigReal rA, rB, rC;	// length of vectors A, B, and C
-  BigReal energy=0;	// energy from the angle
-  BigReal phi;		// angle between the plans
-  BigReal cos_phi;	// cos(phi)
-  BigReal sin_phi;	// sin(phi)
-  Vector dcosdA;	// Derivative d(cos(phi))/dA
-  Vector dcosdB;	// Derivative d(cos(phi))/dB
-  Vector dsindC;	// Derivative d(sin(phi))/dC
-  Vector dsindB;	// Derivative d(sin(phi))/dB
-  BigReal K,K1;		// energy constants
-  BigReal diff;		// for periodicity
-  Force f1(0,0,0),f2(0,0,0),f3(0,0,0);	// force components
+  //  Calculate the vectors between atoms
+  const Position & pos0 = p[0]->x[localIndex[0]].position;
+  const Lattice & lattice = p[0]->p->lattice;
+  const Position & pos1 = p[1]->x[localIndex[1]].position;
+  const Vector r12 = lattice.delta(pos0,pos1);
+  const Position & pos2 = p[2]->x[localIndex[2]].position;
+  const Vector r23 = lattice.delta(pos1,pos2);
+  const Position & pos3 = p[3]->x[localIndex[3]].position;
+  const Vector r34 = lattice.delta(pos2,pos3);
 
-  DebugM(3, "::computeForce() -- starting with dihedral type " << dihedralType << endl);
+  //  Calculate the cross products and distances
+  Vector A = cross(r12,r23);
+  BigReal rA = A.length();
+  Vector B = cross(r23,r34);
+  BigReal rB = B.length();
+  Vector C = cross(r23,A);
+  BigReal rC = C.length();
+
+  //  Calculate the sin and cos
+  BigReal cos_phi = (A*B)/(rA*rB);
+  BigReal sin_phi = (C*B)/(rC*rB);
+
+  BigReal phi= -atan2(sin_phi,cos_phi);
+
+  BigReal K=0;		// energy
+  BigReal K1=0;		// force
 
   // get the dihedral information
   int multiplicity = value->multiplicity;
-
-  //  Calculate the vectors between atoms
-  const Position & pos0 = p[0]->x[localIndex[0]].position;
-  const Position & pos1 = p[1]->x[localIndex[1]].position;
-  const Position & pos2 = p[2]->x[localIndex[2]].position;
-  const Position & pos3 = p[3]->x[localIndex[3]].position;
-  const Lattice & lattice = p[0]->p->lattice;
-  const Vector r12 = lattice.delta(pos0,pos1);
-  const Vector r23 = lattice.delta(pos1,pos2);
-  const Vector r34 = lattice.delta(pos2,pos3);
-
-  //  Calculate the cross products
-  A = cross(r12,r23);
-  B = cross(r23,r34);
-  C = cross(r23,A);
-
-  //  Calculate the distances
-  rA = A.length();
-  rB = B.length();
-  rC = C.length();
-
-  //  Calculate the sin and cos
-  cos_phi = A*B/(rA*rB);
-  sin_phi = C*B/(rC*rB);
-
-  //  Normalize B
-  rB = 1.0/rB;
-  B *= rB;
-
-  phi= -atan2(sin_phi,cos_phi);
-
-  if (fabs(sin_phi) > 0.1)
-  {
-    //  Normalize A
-    rA = 1.0/rA;
-    A *= rA;
-    dcosdA = rA*(cos_phi*A-B);
-    dcosdB = rB*(cos_phi*B-A);
-  }
-  else
-  {
-    //  Normalize C
-    rC = 1.0/rC;
-    C *= rC;
-    dsindC = rC*(sin_phi*C-B);
-    dsindB = rB*(sin_phi*B-C);
-  }
 
   //  Loop through the multiple parameter sets for this
   //  bond.  We will only loop more than once if this
@@ -137,22 +100,26 @@ void DihedralElem::computeForce(BigReal *reduction)
     if (n)
     {
       //  Periodicity is greater than 0, so use cos form
-      K = k*(1+cos(n*phi + delta));
-      K1 = -n*k*sin(n*phi + delta);
+      K += k*(1+cos(n*phi + delta));
+      K1 += -n*k*sin(n*phi + delta);
     }
     else
     {
       //  Periodicity is 0, so just use the harmonic form
-      diff = phi-delta;
+      BigReal diff = phi-delta;
       if (diff < -PI)           diff += TWOPI;
       else if (diff > PI)       diff -= TWOPI;
 
-      K = k*diff*diff;
-      K1 = 2.0*k*diff;
+      K += k*diff*diff;
+      K1 += 2.0*k*diff;
     }
+  } /* for multiplicity */
 
-    //  Add the energy from this dihedral to the total energy
-    energy += K;
+  Force f1,f2,f3;
+
+  //  Normalize B
+  rB = 1.0/rB;
+  B *= rB;
 
     //  Next, we want to calculate the forces.  In order
     //  to do that, we first need to figure out whether the
@@ -161,55 +128,68 @@ void DihedralElem::computeForce(BigReal *reduction)
     if (fabs(sin_phi) > 0.1)
     {
       //  use the sin version to avoid 1/cos terms
+
+      //  Normalize A
+      rA = 1.0/rA;
+      A *= rA;
+      Vector dcosdA = rA*(cos_phi*A-B);
+      Vector dcosdB = rB*(cos_phi*B-A);
+
       K1 = K1/sin_phi;
 
-      f1.x += K1*(r23.y*dcosdA.z - r23.z*dcosdA.y);
-      f1.y += K1*(r23.z*dcosdA.x - r23.x*dcosdA.z);
-      f1.z += K1*(r23.x*dcosdA.y - r23.y*dcosdA.x);
+      f1.x = K1*(r23.y*dcosdA.z - r23.z*dcosdA.y);
+      f1.y = K1*(r23.z*dcosdA.x - r23.x*dcosdA.z);
+      f1.z = K1*(r23.x*dcosdA.y - r23.y*dcosdA.x);
 
-      f3.x += K1*(r23.z*dcosdB.y - r23.y*dcosdB.z);
-      f3.y += K1*(r23.x*dcosdB.z - r23.z*dcosdB.x);
-      f3.z += K1*(r23.y*dcosdB.x - r23.x*dcosdB.y);
+      f3.x = K1*(r23.z*dcosdB.y - r23.y*dcosdB.z);
+      f3.y = K1*(r23.x*dcosdB.z - r23.z*dcosdB.x);
+      f3.z = K1*(r23.y*dcosdB.x - r23.x*dcosdB.y);
 
-      f2.x += K1*(r12.z*dcosdA.y - r12.y*dcosdA.z
+      f2.x = K1*(r12.z*dcosdA.y - r12.y*dcosdA.z
                + r34.y*dcosdB.z - r34.z*dcosdB.y);
-      f2.y += K1*(r12.x*dcosdA.z - r12.z*dcosdA.x
+      f2.y = K1*(r12.x*dcosdA.z - r12.z*dcosdA.x
                + r34.z*dcosdB.x - r34.x*dcosdB.z);
-      f2.z += K1*(r12.y*dcosdA.x - r12.x*dcosdA.y
+      f2.z = K1*(r12.y*dcosdA.x - r12.x*dcosdA.y
              + r34.x*dcosdB.y - r34.y*dcosdB.x);
     }
     else
     {
       //  This angle is closer to 0 or 180 than it is to
       //  90, so use the cos version to avoid 1/sin terms
+
+      //  Normalize C
+      rC = 1.0/rC;
+      C *= rC;
+      Vector dsindC = rC*(sin_phi*C-B);
+      Vector dsindB = rB*(sin_phi*B-C);
+
       K1 = -K1/cos_phi;
 
-      f1.x += K1*((r23.y*r23.y + r23.z*r23.z)*dsindC.x
+      f1.x = K1*((r23.y*r23.y + r23.z*r23.z)*dsindC.x
                 - r23.x*r23.y*dsindC.y
                 - r23.x*r23.z*dsindC.z);
-      f1.y += K1*((r23.z*r23.z + r23.x*r23.x)*dsindC.y
+      f1.y = K1*((r23.z*r23.z + r23.x*r23.x)*dsindC.y
                 - r23.y*r23.z*dsindC.z
                 - r23.y*r23.x*dsindC.x);
-      f1.z += K1*((r23.x*r23.x + r23.y*r23.y)*dsindC.z
+      f1.z = K1*((r23.x*r23.x + r23.y*r23.y)*dsindC.z
                 - r23.z*r23.x*dsindC.x
                 - r23.z*r23.y*dsindC.y);
 
-      f3 += cross(K1,dsindB,r23);
+      f3 = cross(K1,dsindB,r23);
 
-      f2.x += K1*(-(r23.y*r12.y + r23.z*r12.z)*dsindC.x
+      f2.x = K1*(-(r23.y*r12.y + r23.z*r12.z)*dsindC.x
              +(2.0*r23.x*r12.y - r12.x*r23.y)*dsindC.y
              +(2.0*r23.x*r12.z - r12.x*r23.z)*dsindC.z
              +dsindB.z*r34.y - dsindB.y*r34.z);
-      f2.y += K1*(-(r23.z*r12.z + r23.x*r12.x)*dsindC.y
+      f2.y = K1*(-(r23.z*r12.z + r23.x*r12.x)*dsindC.y
              +(2.0*r23.y*r12.z - r12.y*r23.z)*dsindC.z
              +(2.0*r23.y*r12.x - r12.y*r23.x)*dsindC.x
              +dsindB.x*r34.z - dsindB.z*r34.x);
-      f2.z += K1*(-(r23.x*r12.x + r23.y*r12.y)*dsindC.z
+      f2.z = K1*(-(r23.x*r12.x + r23.y*r12.y)*dsindC.z
              +(2.0*r23.z*r12.x - r12.z*r23.x)*dsindC.x
              +(2.0*r23.z*r12.y - r12.z*r23.y)*dsindC.y
              +dsindB.y*r34.x - dsindB.x*r34.y);
     }
-  } /* for multiplicity */
 
   /* store the forces */
   p[0]->f[localIndex[0]] += f1;
@@ -217,8 +197,8 @@ void DihedralElem::computeForce(BigReal *reduction)
   p[2]->f[localIndex[2]] += f3 - f2;
   p[3]->f[localIndex[3]] += -f3;
 
-  DebugM(3, "::computeForce() -- ending with delta energy " << energy << endl);
-  reduction[dihedralEnergyIndex] += energy;
+  DebugM(3, "::computeForce() -- ending with delta energy " << K << endl);
+  reduction[dihedralEnergyIndex] += K;
   reduction[virialIndex_XX] += ( f1.x * r12.x + f2.x * r23.x + f3.x * r34.x );
   reduction[virialIndex_XY] += ( f1.x * r12.y + f2.x * r23.y + f3.x * r34.y );
   reduction[virialIndex_XZ] += ( f1.x * r12.z + f2.x * r23.z + f3.x * r34.z );
