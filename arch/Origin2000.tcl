c8a1985958deb63eb56d777c999f054c6720d2fd
Jim Phillips <jim@ks.uiuc.edu>
2001-01-10 14:26:39 -0600

    Added -lm to fix compile error.

--- arch/Origin2000.tcl
+++ arch/Origin2000.tcl
@@ -1,7 +1,7 @@
 
 TCLDIR=/Projects/namd2/tcl/origin
 TCLINCL=-I$(TCLDIR)/include -I$(HOME)/tcl/include
-TCLLIB=-L$(TCLDIR)/lib -L$(HOME)/tcl/lib -ltcl8.1
+TCLLIB=-L$(TCLDIR)/lib -L$(HOME)/tcl/lib -ltcl8.1 -lm
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
 
