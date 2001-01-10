d7b5577d127db80231a2cc6d56a45c08170c1792
Jim Phillips <jim@ks.uiuc.edu>
2001-01-10 15:15:49 -0600

    Fixes for PSC TCSini.

--- arch/Tru64-Alpha.tcl
+++ arch/Tru64-Alpha.tcl
@@ -1,7 +1,7 @@
 
 TCLDIR=/Projects/namd2/tcl/alpha
-TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.1
+TCLINCL=-I$(TCLDIR)/include -I$(HOME)/tcl/include
+TCLLIB=-L$(TCLDIR)/lib -L$(HOME)/tcl/lib -ltcl8.1
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
 
