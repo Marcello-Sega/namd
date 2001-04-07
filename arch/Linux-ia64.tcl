269d015bcd4c243984fed25f9b7ddd9832c62816
Gengbin Zheng <gzheng@ks.uiuc.edu>
2001-04-07 16:21:47 -0500

    add port to Linux IA64.

---
+++ arch/Linux-ia64.tcl
@@ -0,0 +1,7 @@
+
+TCLDIR=/Projects/namd2/tcl/linux
+TCLINCL=-I$(TCLDIR)/include -I$(HOME)/tcl-linux/include
+TCLLIB=-L$(TCLDIR)/lib -L$(HOME)/tcl-linux/lib -ltcl8.1
+TCLFLAGS=-DNAMD_TCL
+TCL=$(TCLINCL) $(TCLFLAGS)
+
