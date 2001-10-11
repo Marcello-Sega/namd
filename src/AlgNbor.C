267026393f6401ec2eeb7dcf55b831c7718fa768
Gengbin Zheng <gzheng@ks.uiuc.edu>
2001-10-11 13:28:23 -0500

    added first version of Namd neigbor load balancing, not fully completed yet.

---
+++ src/AlgNbor.C
@@ -0,0 +1,186 @@
+/**
+***  Copyright (c) 1995, 1996, 1997, 1998, 1999, 2000 by
+***  The Board of Trustees of the University of Illinois.
+***  All rights reserved.
+**/
+
+#include <iostream.h>
+#include "common.h"
+#include "InfoStream.h"
+#include "AlgNbor.h"
+
+#define TINYLOAD 0.0005
+
+AlgNbor::AlgNbor(int pe, computeInfo *computeArray, patchInfo *patchArray, 
+	   processorInfo *processorArray, int nComps, 
+	   int nPatches, int nPes, int nNbs) :
+  Rebalancer(computeArray, patchArray, 
+	     processorArray, nComps, 
+	     nPatches, nPes)
+{
+strategyName = "AlgNbor";
+mype = pe;
+nNbors = nNbs;
+strategy();
+}
+
+void AlgNbor::strategy()
+{
+  double startTime = CmiWallTimer();
+
+  // make initial assignment as it is now
+  for (int i=0; i<numComputes; i++) {
+        assign((computeInfo *) &(computes[i]),
+		 (processorInfo *) &(processors[computes[i].oldProcessor]));
+  }
+
+  // calculate avarage load for neighbors
+  double myload = processors[mype].load;
+  double avgload = 0.0;
+  int i;
+  for (i=0; i<P; i++) {
+    if (processors[i].Id >= 0) {
+CmiPrintf("[%d] %d:%f\n", CmiMyPe(), i, processors[i].load);
+      avgload += processors[i].load;
+    }
+  }
+  avgload /= (nNbors+1);
+
+  CmiPrintf("Myload: %f, avrage load: %f. \n", myload, avgload);
+  if (myload <= avgload) return;
+
+  minHeap procs(nNbors+1);
+  for (i=0; i<P; i++) {
+    if (processors[i].Id >= 0 && processors[i].Id != mype) 
+      procs.insert(&processors[i]);
+  }
+
+  maxHeap objs(numComputes);
+  for (i=0; i< numComputes; i++) {
+    if (computes[i].oldProcessor == mype)
+      objs.insert(&computes[i]);
+  }
+
+  do {
+    processorInfo *p;
+    computeInfo *c;
+
+    p = (processorInfo *)procs.deleteMin();
+    if (p == NULL) break;
+    CmiBool objfound = CmiFalse;
+    do {
+      c = (computeInfo *)objs.deleteMax();
+      if (c == NULL) break;
+
+      double new_p_load = p->load + c->load;
+      double my_new_load = myload - c->load;
+//CmiPrintf("new load: new_p_load:%e my_new_load:%e c:%e\n", new_p_load, my_new_load, c->load);
+      if (new_p_load < my_new_load) {
+	objfound = CmiTrue;
+      } 
+    } while (!objfound);
+    if (!objfound) break;
+    deAssign(c, &processors[mype]);
+    assign(c, p);
+    procs.insert(p);
+  } while (myload > avgload);
+/*
+  // double bestSize0, bestSize1, bestSize2;
+  computeInfo *c;
+  int numAssigned;
+  processorInfo* goodP[3][3];  // goodP[# of real patches][# of proxies]
+  processorInfo* poorP[3][3];  // fallback option
+
+
+  //   iout << iINFO  << "calling makeHeaps. \n";
+  makeHeaps();
+  computeAverage();
+  //   iout << iINFO
+  //	<< "Before assignment\n" << endi;
+  //   printLoads();
+	      
+  numAssigned = 0;
+
+  //   for (int i=0; i<numPatches; i++)
+  //     { cout << "(" << patches[i].Id << "," << patches[i].processor ;}
+  overLoad = 1.2;
+  for (int ic=0; ic<numComputes; ic++) {
+    c = (computeInfo *) computesHeap->deleteMax();
+    if ( ! c ) NAMD_bug("AlgNbor: computesHeap empty!");
+    if (c->processor != -1) continue; // skip to the next compute;
+    heapIterator nextProcessor;
+    processorInfo *p = (processorInfo *) 
+      pes->iterator((heapIterator *) &nextProcessor);
+    int i,j;
+    for(i=0;i<3;i++)
+      for(j=0;j<3;j++) {
+	goodP[i][j]=0;
+	poorP[i][j]=0;
+      }
+    while (p) {
+      int nPatches = numPatchesAvail(c,p);
+      int nProxies = numProxiesAvail(c,p);
+      if (nPatches < 0 || nPatches > 2)
+	iout << iERROR << "Too many patches: " << nPatches << "\n" << endi;
+      if (nProxies < 0 || nProxies > 2)
+	iout << iERROR << "Too many proxies: " << nProxies << "\n" << endi;
+
+      if (!goodP[nPatches][nProxies] ||
+	    (p->load < goodP[nPatches][nProxies]->load)) {
+        if (c->load + p->load < overLoad*averageLoad) {
+	  goodP[nPatches][nProxies] = p;
+        }
+      }
+      if (!poorP[nPatches][nProxies] ||
+	    (p->load < poorP[nPatches][nProxies]->load)) {
+	poorP[nPatches][nProxies] = p;   // fallback
+      }
+      p = (processorInfo *) pes->next(&nextProcessor);
+    }
+
+    //    if (numAssigned >= 0) {  Else is commented out below
+
+    p = 0;
+    if ((p = goodP[2][0])    // Two home, no proxies
+     || (p = goodP[1][1])    // One home, one proxy
+     || (p = goodP[0][2])    // No home, two proxies
+     || (p = goodP[1][0])    // One home, no proxies
+     || (p = goodP[0][1])    // No home, one proxy
+     || (p = goodP[0][0])    // No home, no proxies
+     || (p = poorP[2][0])    // Two home, no proxies, overload
+     || (p = poorP[1][1])    // One home, one proxy, overload
+     || (p = poorP[0][2])    // No home, two proxies, overload
+     || (p = poorP[1][0])    // One home, no proxies, overload
+     || (p = poorP[0][1])    // No home, one proxy, overload
+     || (p = poorP[0][0])    // No home, no proxies, overload
+       ) {
+      assign(c,p); numAssigned++;
+    } else {
+      NAMD_bug("*** Alg 7 No receiver found 1 ***");
+      break;
+    }
+
+  }
+
+
+  // binary-search refinement procedure
+  multirefine();
+*/
+
+  CmiPrintf("AlgNbor finish time: %f.\n", CmiWallTimer()-startTime);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/NamdNborLB.C
@@ -0,0 +1,304 @@
+
+#if !defined(WIN32) || defined(__CYGWIN__)
+#include <unistd.h>
+#endif
+#include <fcntl.h>
+
+#include <charm++.h>
+
+#include "NamdNborLB.h"
+#include "NamdNborLB.def.h"
+#include "Node.h"
+#include "PatchMap.h"
+#include "ComputeMap.h"
+#include "LdbCoordinator.h"
+
+void CreateNamdNborLB()
+{
+  // CkPrintf("[%d] creating NamdNborLB %d\n",CkMyPe(),loadbalancer);
+  nborBaselb = CProxy_NamdNborLB::ckNew();
+  // CkPrintf("[%d] created NamdNborLB %d\n",CkMyPe(),loadbalancer);
+}
+
+NamdNborLB::NamdNborLB()
+{
+  //  if (CkMyPe()==0)
+  //   CkPrintf("[%d] NamdNborLB created\n",CkMyPe());
+  processorArray = 0;
+  patchArray = 0;
+  computeArray = 0;
+}
+
+/*
+NamdNborLB::~NamdNborLB()
+{
+  delete [] processorArray;
+  delete [] patchArray;
+  delete [] computeArray;
+}
+*/
+
+CmiBool NamdNborLB::QueryBalanceNow(int _step)
+{
+  //  CkPrintf("[%d] Balancing on step %d\n",CkMyPe(),_step);
+  if ( LdbCoordinator::Object()->takingLdbData ) {
+    return CmiTrue;
+  } else {
+    return CmiFalse;
+  }
+}
+
+NLBMigrateMsg* NamdNborLB::Strategy(NborBaseLB::LDStats* stats, int count)
+{
+  //  CkPrintf("LDB: All statistics received at %f, %f\n",
+  //  CmiTimer(),CmiWallTimer());
+  int i,j;
+
+  const int numProcessors = CmiNumPes();
+  const int numPatches = PatchMap::Object()->numPatches();
+  const int numComputes = ComputeMap::Object()->numComputes();
+  const SimParameters* simParams = Node::Object()->simParameters;
+
+  int nMoveableComputes = 0;
+  for (i=0; i < count+1; i++) {
+    LDStats &thisLDStats = (i==count)?myStats:stats[i];
+    for (j=0; j < thisLDStats.n_objs; j++) {
+      const LDObjData this_obj = thisLDStats.objData[j];
+      // filter out non-NAMD managed objects (like PME array)
+      if (this_obj.omID.id != 1) continue;
+      if (this_obj.migratable)  nMoveableComputes++;
+    }
+  }
+
+  // these sizes should never change
+  if ( ! processorArray ) processorArray = new processorInfo[numProcessors];
+  if ( ! patchArray ) patchArray = new patchInfo[numPatches];
+  if ( ! computeArray ) computeArray = new computeInfo[nMoveableComputes];
+
+  nMoveableComputes = buildData(stats,count);
+
+  AlgNbor(CkMyPe(), computeArray,patchArray,processorArray,
+			nMoveableComputes, numPatches, numProcessors, count);
+/*
+  if (simParams->ldbStrategy == LDBSTRAT_REFINEONLY) {
+    RefineOnly(computeArray,patchArray,processorArray,
+                                nMoveableComputes, numPatches, numProcessors);
+  } else if (simParams->ldbStrategy == LDBSTRAT_ALG7) {
+    Alg7(computeArray,patchArray,processorArray,
+                          nMoveableComputes, numPatches, numProcessors);
+  } else if (simParams->ldbStrategy == LDBSTRAT_ALGROB) {
+    if (step() == 0) {
+      iout << iINFO << "Load balance cycle " << step()
+        << " using RecBisection\n" << endi;
+      AlgRecBisection(computeArray,patchArray,processorArray,
+                            nMoveableComputes, numPatches, numProcessors);
+    } else {
+      iout << iINFO << "Load balance cycle " << step()
+        << " using RefineOnly\n" << endi;
+      RefineOnly(computeArray,patchArray,processorArray,
+                                  nMoveableComputes, numPatches,
+                                  numProcessors);
+    }
+  } else if (simParams->ldbStrategy == LDBSTRAT_OTHER) {
+    if (step() == 0) {
+      iout << iINFO << "Load balance cycle " << step()
+        << " using Alg7\n" << endi;
+      Alg7(computeArray,patchArray,processorArray,
+                            nMoveableComputes, numPatches, numProcessors);
+    } else {
+      iout << iINFO << "Load balance cycle " << step()
+        << " using RefineOnly\n" << endi;
+      // To save the data to a file, uncomment the following lines -RKB
+      //      if (step() == 1) {
+      //	iout << iINFO << "Dumping data\n" << endi;
+      //	dumpDataASCII("refinedata", numProcessors, numPatches,
+      //		      nMoveableComputes);
+      //      }
+      RefineOnly(computeArray,patchArray,processorArray,
+                                  nMoveableComputes, numPatches,
+                                  numProcessors);
+    }
+  }
+*/
+
+  // For error checking:
+  // Count up computes, to see if somebody doesn't have any computes
+/*
+  int* computeCount = new int[numProcessors];
+  for(i=0; i<numProcessors; i++)
+    computeCount[i]=0;
+  for(i=0; i<nMoveableComputes; i++)
+    computeCount[computeArray[i].processor]++;
+  for(i=0; i<numProcessors; i++) {
+    if (computeCount[i]==0)
+      iout << iINFO <<"Warning: Processor " << i 
+	   << " has NO moveable computes.\n" << endi;
+  }
+  delete [] computeCount;
+*/
+  
+  CkVec<MigrateInfo *> migrateInfo;
+  for(i=0;i<nMoveableComputes;i++) {
+    if (computeArray[i].oldProcessor == CkMyPe())
+    if (computeArray[i].processor != computeArray[i].oldProcessor) {
+            CkPrintf("[%d] Obj %d migrating from %d to %d\n",
+                     CkMyPe(),computeArray[i].handle.id.id[0],
+      	       computeArray[i].processor,computeArray[i].oldProcessor);
+      MigrateInfo *migrateMe = new MigrateInfo;
+      migrateMe->obj = computeArray[i].handle;
+      migrateMe->from_pe = computeArray[i].oldProcessor;
+      migrateMe->to_pe = computeArray[i].processor;
+      migrateInfo.insertAtEnd(migrateMe);
+    }
+  }
+  
+  int migrate_count=migrateInfo.length();
+  CkPrintf("NamdNborLB [%d] migrating %d elements\n", CkMyPe(), migrate_count);
+  NLBMigrateMsg* msg = new(&migrate_count,1) NLBMigrateMsg;
+  msg->n_moves = migrate_count;
+  for(i=0; i < migrate_count; i++) {
+    MigrateInfo* item = migrateInfo[i];
+    msg->moves[i] = *item;
+    delete item;
+    migrateInfo[i] = 0;
+  }
+  return msg;
+};
+
+
+int NamdNborLB::buildData(NborBaseLB::LDStats* stats, int count)
+{
+  PatchMap* patchMap = PatchMap::Object();
+  ComputeMap* computeMap = ComputeMap::Object();
+  double bg_weight = 0.7;
+
+  int i;
+  for (i=0; i<CmiNumPes(); i++) {
+    int peslot = NeighborIndex(i);
+    if (i == CmiMyPe()) {
+      processorArray[i].Id = i;
+    if (patchMap->numPatches() > 0)
+      processorArray[i].backgroundLoad = myStats.bg_walltime*bg_weight;
+    else 
+      processorArray[i].backgroundLoad = myStats.bg_walltime;
+      continue;
+    }
+    if (peslot != -1) {
+    processorArray[i].Id = i;
+    if (patchMap->numPatches() > 0)
+      processorArray[i].backgroundLoad = bg_weight * stats[peslot].bg_walltime;
+    else 
+      processorArray[i].backgroundLoad = stats[peslot].bg_walltime;
+    }
+    else 
+      processorArray[i].Id = -2;
+  }
+
+  int nMoveableComputes=0;
+  int nProxies = 0;		// total number of estimated proxies
+  for (i=0; i<patchMap->numPatches(); i++) {
+	patchArray[i].Id = i;
+	patchArray[i].numAtoms = 0;
+	patchArray[i].processor = patchMap->node(i);
+	int neighborNodes[PatchMap::MaxOneAway + PatchMap::MaxTwoAway];
+	const int numProxies = requiredProxies(i,neighborNodes);
+        nProxies += numProxies;
+
+	for (int k=0; k<numProxies; k++) {
+	  if (NeighborIndex(neighborNodes[k]) != -1) {
+	    processorArray[neighborNodes[k]].proxies.insert(&patchArray[i]);
+	    patchArray[i].proxiesOn.insert(&processorArray[neighborNodes[k]]);
+	  }
+	}
+  }
+  // TODO: myStats need to fill
+  for (i=0; i < count+1; i++) {
+    int j;
+    LDStats &thisLDStats = (i==count)?myStats:stats[i];
+    for (j=0; j < thisLDStats.n_objs; j++) {
+      const LDObjData this_obj = thisLDStats.objData[j];
+      // filter out non-NAMD managed objects (like PME array)
+      if (this_obj.omID.id != 1) continue;
+      if (this_obj.id.id[1] == -2) { // Its a patch
+/*
+	const int pid = this_obj.id.id[0];
+	int neighborNodes[PatchMap::MaxOneAway + PatchMap::MaxTwoAway];
+
+	patchArray[pid].Id = pid;
+	patchArray[pid].numAtoms = 0;
+	patchArray[pid].processor = i;
+	const int numProxies = requiredProxies(pid,neighborNodes);
+        nProxies += numProxies;
+
+	for (int k=0; k<numProxies; k++) {
+	  processorArray[neighborNodes[k]].proxies.insert(&patchArray[pid]);
+	  patchArray[pid].proxiesOn.insert(&processorArray[neighborNodes[k]]);
+	}
+*/
+      } else if (this_obj.migratable) { // Its a compute
+	const int cid = this_obj.id.id[0];
+	const int p0 = computeMap->pid(cid,0);
+
+	// For self-interactions, just return the same pid twice
+	int p1;
+	if (computeMap->numPids(cid) > 1)
+	  p1 = computeMap->pid(cid,1);
+	else p1 = p0;
+	computeArray[nMoveableComputes].Id = cid;
+	computeArray[nMoveableComputes].oldProcessor = thisLDStats.from_pe;
+	computeArray[nMoveableComputes].processor = -1;
+	computeArray[nMoveableComputes].patch1 = p0;
+	computeArray[nMoveableComputes].patch2 = p1;
+	computeArray[nMoveableComputes].handle = this_obj.handle;
+	computeArray[nMoveableComputes].load = this_obj.wallTime;
+	nMoveableComputes++;
+      }
+    }
+  }
+  return nMoveableComputes;
+}
+
+// Figure out which proxies we will definitely create on other
+// nodes, without regard for non-bonded computes.  This code is swiped
+// from ProxyMgr, and changes there probable need to be propagated here.
+
+int NamdNborLB::requiredProxies(PatchID id, int neighborNodes[])
+{
+  enum proxyHere { No, Yes };
+  int numNodes = CkNumPes();
+  proxyHere *proxyNodes = new proxyHere[numNodes];
+  int nProxyNodes;
+  int i;
+
+  // Note all home patches.
+  for ( i = 0; i < numNodes; ++i )
+  {
+    proxyNodes[i] = No;
+  }
+  nProxyNodes=0;
+
+  // Check all two-away neighbors.
+  // This is really just one-away neighbors, since 
+  // two-away always returns zero: RKB
+  PatchID neighbors[PatchMap::MaxOneAway + PatchMap::MaxTwoAway];
+
+  PatchMap* patchMap = PatchMap::Object();
+
+  int myNode = patchMap->node(id);
+  int numNeighbors = patchMap->downstreamNeighbors(id,neighbors);
+  for ( i = 0; i < numNeighbors; ++i )
+  {
+    const int proxyNode = patchMap->node(neighbors[i]);
+    if (proxyNode != myNode)
+      if (proxyNodes[proxyNode] == No)
+      {
+	proxyNodes[proxyNode] = Yes;
+	neighborNodes[nProxyNodes] = proxyNode;
+	nProxyNodes++;
+      }
+  }
+
+  delete [] proxyNodes;
+  return nProxyNodes;
+}
+
