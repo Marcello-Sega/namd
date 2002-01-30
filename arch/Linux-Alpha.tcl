0c1a4669d9413452a19910618b82bfc80b6fe9f6
Jim Phillips <jim@ks.uiuc.edu>
2002-01-30 17:48:28 -0600

    Add -ldl for Linux.

--- arch/Linux-Alpha.tcl
+++ arch/Linux-Alpha.tcl
@@ -1,7 +1,7 @@
 
 TCLDIR=/Projects/namd2/tcl/alphalinux
 TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.3
+TCLLIB=-L$(TCLDIR)/lib -ltcl8.3 -ldl
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
 
--- arch/Linux-i686.tcl
+++ arch/Linux-i686.tcl
@@ -1,7 +1,7 @@
 
 TCLDIR=/Projects/namd2/tcl/linux
 TCLINCL=-I$(TCLDIR)/include -I$(HOME)/tcl-linux/include
-TCLLIB=-L$(TCLDIR)/lib -L$(HOME)/tcl-linux/lib -ltcl8.3
+TCLLIB=-L$(TCLDIR)/lib -L$(HOME)/tcl-linux/lib -ltcl8.3 -ldl
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
 
