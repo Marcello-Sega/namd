9741e0552154cc7f6111668fd0e1cfdfbca156c9
Jim Phillips <jim@ks.uiuc.edu>
2002-06-03 15:17:53 -0500

    fixed warning about testing for unsigned < 0

--- src/dcdlib.C
+++ src/dcdlib.C
@@ -39,7 +39,6 @@
 // same as write, only does error checking internally
 void NAMD_write(int fd, const char *buf, size_t count) {
   while ( count ) {
-    if ( count < 0 ) NAMD_bug("count < 0 in NAMD_write64()");
     ssize_t retval =
 #if defined(WIN32) && !defined(__CYGWIN__)
    	_write(fd,buf,count);
@@ -47,6 +46,7 @@ void NAMD_write(int fd, const char *buf, size_t count) {
    	write(fd,buf,count);
 #endif
     if ( retval < 0 ) NAMD_die(strerror(errno));
+    if ( retval > count ) NAMD_bug("extra bytes written in NAMD_write64()");
     buf += retval;
     count -= retval;
   }
