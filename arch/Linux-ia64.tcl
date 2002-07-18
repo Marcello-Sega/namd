2587c40965b52e3a52384e158ead5a695bc1c580
Jim Phillips <jim@ks.uiuc.edu>
2002-07-18 16:28:53 -0500

    IA64 options.

--- arch/Linux-ia64.tcl
+++ arch/Linux-ia64.tcl
@@ -1,7 +1,7 @@
 
 TCLDIR=/Projects/namd2/tcl/linux
-TCLINCL=-I$(TCLDIR)/include -I$(HOME)/tcl-linux/include
-TCLLIB=-L$(TCLDIR)/lib -L$(HOME)/tcl-linux/lib -ltcl8.1
+TCLINCL=-I$(TCLDIR)/include -I$(HOME)/tcl/include
+TCLLIB=-L$(TCLDIR)/lib -L$(HOME)/tcl/lib -ltcl8.3 -ldl
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
 
