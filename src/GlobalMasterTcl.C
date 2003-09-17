a8da91baf02fb1856db3cb5aa7ba0d5370454dbf
Fangqiang Zhu <fzhu@ks.uiuc.edu>
2003-09-17 11:02:50 -0500

    A correction for the implementation of "addenergy", so that it can appear multiple times in calcforces{}.

--- src/GlobalMasterTcl.C
+++ src/GlobalMasterTcl.C
@@ -415,7 +415,6 @@ int GlobalMasterTcl::Tcl_addenergy(ClientData clientData,
   
   GlobalMasterTcl *self = (GlobalMasterTcl *)clientData;
   self->reduction->item(REDUCTION_MISC_ENERGY) += energy;
-  self->reduction->submit();
   
   return TCL_OK;
 }
@@ -554,6 +553,8 @@ void GlobalMasterTcl::calculate() {
     char *errorInfo = Tcl_GetVar(interp,"errorInfo",0);
     NAMD_die(errorInfo);
   }
+  if (reduction->item(REDUCTION_MISC_ENERGY) != 0)
+    reduction->submit();
 #endif
 
 }
