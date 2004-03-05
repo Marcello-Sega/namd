f5eac3b951abcf27d7db41f6a47212ece4470631
Jim Phillips <jim@ks.uiuc.edu>
2004-03-05 11:17:25 -0600

    Fixed NAMD_PLATFORM bug, added NAMD_SUBARCH variable.

---
+++ arch/Linux-i686-redhat9.tcl
@@ -0,0 +1,7 @@
+
+TCLDIR=/Projects/namd2/tcl/linux-redhat9
+TCLINCL=-I$(TCLDIR)/include -I$(HOME)/tcl/include
+TCLLIB=-L$(TCLDIR)/lib -L$(HOME)/tcl/lib -ltcl8.3 -ldl
+TCLFLAGS=-DNAMD_TCL -DUSE_NON_CONST
+TCL=$(TCLINCL) $(TCLFLAGS)
+
