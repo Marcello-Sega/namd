a5c9c6bf4f2727637ba0f3cadc064fe1b98a1e57
Gengbin Zheng <gzheng@ks.uiuc.edu>
2004-10-27 16:40:55 -0500

    config files for red storm (official name Cray XT3)

--- arch/CRAY-XT3.tcl
+++ arch/CRAY-XT3.tcl
@@ -1,8 +1,5 @@
-
-TCLDIR=/Projects/namd2/tcl/linux
-#TCLDIR=$(HOME)/tcl/cray-xt3
+TCLDIR=$(HOME)/tcl/cray-xt3
 TCLINCL=-I$(TCLDIR)/include -I$(HOME)/tcl/include
 TCLLIB=-L$(TCLDIR)/lib -L$(HOME)/tcl/lib -ltcl8.3 -ldl
-TCLFLAGS=-DNAMD_TCL -DUSE_NON_CONST
+TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
-
