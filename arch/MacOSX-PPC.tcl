e689b8e4985323ae6ff4742140c368b6271b9cd7
Jim Phillips <jim@ks.uiuc.edu>
2005-07-29 18:00:38 -0500

    Force static tcl library for portability.

--- arch/MacOSX-PPC.tcl
+++ arch/MacOSX-PPC.tcl
@@ -1,7 +1,7 @@
 
 TCLDIR=/Projects/namd2/tcl/darwin
 TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.3
+TCLLIB=-L$(TCLDIR)/lib -lnamdtcl8.3
 TCLFLAGS=-DNAMD_TCL -DUSE_NON_CONST
 TCL=$(TCLINCL) $(TCLFLAGS)
 
