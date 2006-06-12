9e16a64cb06d9b485b13046628ffb6328173e0f6
Jim Phillips <jim@ks.uiuc.edu>
2006-06-12 17:37:51 -0500

    *** empty log message ***

--- src/memusage.C
+++ src/memusage.C
@@ -7,6 +7,8 @@
 #include "memusage.h"
 #include "converse.h"
 #ifndef WIN32
+#include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
 #else
 int sbrk(int) { return 0; }
@@ -27,50 +29,69 @@ long memusageinit::memusage_sbrk() {
   return ( newval - memusageinit::sbrkval );
 }
 
+
 #ifdef WIN32
 #define MEMUSAGE_USE_SBRK
 #endif
 
-#ifdef MAC_OSX
+#ifdef _NO_MALLOC_H
+#define NO_MALLINFO
+#endif
+
+#ifdef MEMUSAGE_USE_SBRK
+#define NO_MALLINFO
+#define NO_PS
+#endif
+
+
+#ifdef MEMUSAGE_USE_MSTATS
 
 #include <malloc/malloc.h>
 
 long memusage_mstats() {
-
   struct mstats ms = mstats();
-
   long memtotal = ms.bytes_used;
-
   return memtotal;
-
 }
+
 #else
 inline long memusage_mstats() { return 0; }
 #endif
 
-#ifdef _NO_MALLOC_H
-#define MEMUSAGE_USE_SBRK
-#endif
 
-#ifndef MEMUSAGE_USE_SBRK
+#ifndef NO_MALLINFO
 
 #include <malloc.h>
 
 long memusage_mallinfo() {
-
   struct mallinfo mi = mallinfo();
-
   long memtotal = mi.usmblks + mi.uordblks + mi.hblkhd;
-
   return memtotal;
-
 }
 
-#else // ifndef MEMUSAGE_USE_SBRK
-
+#else
 inline long memusage_mallinfo() { return 0; }
-
 #endif
+
+
+#ifndef NO_PS
+
+inline long memusage_ps() {
+  char pscmd[100];
+  sprintf(pscmd, "/bin/ps -o vsz= -p %d", getpid());
+  long vsz = 0;
+  FILE *p = popen(pscmd, "r");
+  if ( p ) {
+    fscanf(p, "%ld", &vsz);
+    pclose(p);
+  }
+  return ( vsz * 1024 );
+}
+
+#else
+inline long memusage_ps() { return 0; }
+#endif
+
 
 long memusage() {
 
@@ -86,6 +107,7 @@ long memusage() {
 
   if ( ! memtotal ) memtotal = memusageinit::memusage_sbrk();
 
+  if ( ! memtotal ) memtotal = memusage_ps();
 
   return memtotal;
 
