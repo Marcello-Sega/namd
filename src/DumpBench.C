714ba7bf580634f661e75695e96ff7f05c8bb085
Chao Mei <chaomei2@ks.uiuc.edu>
2007-03-16 16:57:11 -0500

    1. Change dumping to use atom signatures and exclusion check signatures 2. Adding dumping for bonds/angles/dihedrals/impropers

--- src/DumpBench.C
+++ src/DumpBench.C
@@ -63,13 +63,28 @@ int dumpbench(FILE *file) {
   const Molecule *mol = node->molecule;
 
   fprintf(file,"%d %d\n",mol->numAtoms,mol->numCalcExclusions);
-
+ 
   for ( i=0; i<mol->numAtoms; ++i) {
     int vdw = mol->atomvdwtype(i);
+    #ifdef MEM_OPT_VERSION
+    Index exclIdx = mol->getAtomExclSigId(i);
+    const ExclusionCheck *excl = mol->get_excl_check_for_idx(exclIdx);
+    if(excl->flags==NULL || excl->flags == (char *)-1){
+        fprintf(file,"%d: %d ====\n",i, vdw);
+        continue;
+    }
+    int min = i+excl->min;
+    int max = i+excl->max;
+    #else
     const ExclusionCheck *excl = mol->get_excl_check_for_atom(i);
+    if(excl->flags==NULL || excl->flags == (char *)-1){
+        fprintf(file,"%d: %d ====\n",i, vdw);
+        continue;
+    }
     int min = excl->min;
     int max = excl->max;
-    fprintf(file,"%d %d %d",vdw,min,max);
+    #endif
+    fprintf(file,"%d: %d %d %d |",i,vdw,min,max);
     if ( min <= max ) {
       int s = max - min + 1;
       const char *f = excl->flags;
@@ -82,6 +97,154 @@ int dumpbench(FILE *file) {
   }
 
   fprintf(file,"MOLECULE_END\n");
+
+  fprintf(file, "BONDS_BEGIN\n");
+  fprintf(file, "%d %d\n", mol->numBonds, mol->numCalcBonds);
+#ifdef MEM_OPT_VERSION
+  for(i=0; i<mol->numAtoms; i++){
+      int sigId = node->molecule->getAtomSigId(i);
+      AtomSignature *sig = &(mol->atomSigPool[sigId]);
+      if(sig->bondCnt==0){
+          fprintf(file, "%d: ===\n", i);
+          continue;
+      }
+      fprintf(file, "%d:", i);
+      for(j=0; j<sig->bondCnt; j++){
+          fprintf(file, " (%d | %d)", (sig->bondSigs[j]).offset[0], sig->bondSigs[j].tupleParamType);
+      }
+      fprintf(file, "\n");
+  }
+#else
+  for(i=0; i<mol->numAtoms; i++){      
+      int *p = node->molecule->get_bonds_for_atom(i);      
+      if(*p==-1){
+          fprintf(file, "%d: ===\n", i);
+          continue;
+      }
+      fprintf(file, "%d:", i);
+      for(; *p!=-1;p++){
+          Bond *t = mol->get_bond(*p);
+          fprintf(file, " (%d | %d)", t->atom2-i, t->bond_type);
+      }
+      fprintf(file, "\n");
+  }
+#endif
+  fprintf(file, "BONDS_END\n");
+
+  fprintf(file, "ANGLES_BEGIN\n");
+  fprintf(file, "%d %d\n", mol->numAngles, mol->numCalcAngles);
+#ifdef MEM_OPT_VERSION
+  for(i=0; i<mol->numAtoms; i++){
+      int sigId = node->molecule->getAtomSigId(i);
+      AtomSignature *sig = &(mol->atomSigPool[sigId]);
+      if(sig->angleCnt==0){
+          fprintf(file, "%d: ===\n", i);
+          continue;
+      }
+      fprintf(file, "%d:", i);
+      for(j=0; j<sig->angleCnt; j++){
+          int offset0 = (sig->angleSigs[j]).offset[0];
+          int offset1 = (sig->angleSigs[j]).offset[1];
+          fprintf(file, " (%d, %d | %d)", offset0, offset1, sig->angleSigs[j].tupleParamType);
+      }
+      fprintf(file, "\n");
+  }
+#else
+  for(i=0; i<mol->numAtoms; i++){      
+      int *p = node->molecule->get_angles_for_atom(i);      
+      if(*p==-1){
+          fprintf(file, "%d: ===\n", i);
+          continue;
+      }
+      fprintf(file, "%d:", i);
+      for(; *p!=-1;p++){
+          Angle *t = mol->get_angle(*p);
+          int offset0 = t->atom2 - i;
+          int offset1 = t->atom3 - i;
+          fprintf(file, " (%d, %d | %d)", offset0, offset1, t->angle_type);
+      }
+      fprintf(file, "\n");
+  }
+#endif
+  fprintf(file, "ANGLES_END\n");
+
+  fprintf(file, "DIHEDRALS_BEGIN\n");
+  fprintf(file, "%d %d\n", mol->numDihedrals, mol->numCalcDihedrals);
+#ifdef MEM_OPT_VERSION
+  for(i=0; i<mol->numAtoms; i++){
+      int sigId = node->molecule->getAtomSigId(i);
+      AtomSignature *sig = &(mol->atomSigPool[sigId]);
+      if(sig->dihedralCnt==0){
+          fprintf(file, "%d: ===\n", i);
+          continue;
+      }
+      fprintf(file, "%d:", i);
+      for(j=0; j<sig->dihedralCnt; j++){
+          int offset0 = (sig->dihedralSigs[j]).offset[0];
+          int offset1 = (sig->dihedralSigs[j]).offset[1];
+          int offset2 = (sig->dihedralSigs[j]).offset[2];
+          fprintf(file, " (%d, %d, %d | %d)", offset0, offset1, offset2, sig->dihedralSigs[j].tupleParamType);
+      }
+      fprintf(file, "\n");
+  }
+#else
+  for(i=0; i<mol->numAtoms; i++){      
+      int *p = node->molecule->get_dihedrals_for_atom(i);      
+      if(*p==-1){
+          fprintf(file, "%d: ===\n", i);
+          continue;
+      }
+      fprintf(file, "%d:", i);
+      for(; *p!=-1;p++){
+          Dihedral *t = mol->get_dihedral(*p);
+          int offset0 = t->atom2 - i;
+          int offset1 = t->atom3 - i;
+          int offset2 = t->atom4 - i;
+          fprintf(file, " (%d, %d, %d | %d)", offset0, offset1, offset2, t->dihedral_type);
+      }
+      fprintf(file, "\n");
+  }
+#endif
+  fprintf(file, "DIHEDRALS_END\n");
+
+  fprintf(file, "IMPROPERS_BEGIN\n");
+  fprintf(file, "%d %d\n", mol->numImpropers, mol->numCalcImpropers);
+#ifdef MEM_OPT_VERSION
+  for(i=0; i<mol->numAtoms; i++){
+      int sigId = node->molecule->getAtomSigId(i);
+      AtomSignature *sig = &(mol->atomSigPool[sigId]);
+      if(sig->improperCnt==0){
+          fprintf(file, "%d: ===\n", i);
+          continue;
+      }
+      fprintf(file, "%d:", i);
+      for(j=0; j<sig->improperCnt; j++){
+          int offset0 = (sig->improperSigs[j]).offset[0];
+          int offset1 = (sig->improperSigs[j]).offset[1];
+          int offset2 = (sig->improperSigs[j]).offset[2];
+          fprintf(file, " (%d, %d, %d | %d)", offset0, offset1, offset2, sig->improperSigs[j].tupleParamType);
+      }
+      fprintf(file, "\n");
+  }
+#else
+  for(i=0; i<mol->numAtoms; i++){      
+      int *p = node->molecule->get_impropers_for_atom(i);      
+      if(*p==-1){
+          fprintf(file, "%d: ===\n", i);
+          continue;
+      }
+      fprintf(file, "%d:", i);
+      for(; *p!=-1;p++){
+          Improper *t = mol->get_improper(*p);
+          int offset0 = t->atom2 - i;
+          int offset1 = t->atom3 - i;
+          int offset2 = t->atom4 - i;
+          fprintf(file, " (%d, %d, %d | %d)", offset0, offset1, offset2, t->improper_type);
+      }
+      fprintf(file, "\n");
+  }
+#endif
+  fprintf(file, "IMPROPERS_END\n");
 
   fprintf(file,"PATCHLIST_BEGIN\n");
 
