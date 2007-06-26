18a7c55ac2ffb9c806d4d5895ea3cad2e9c21556
Chao Mei <chaomei2@ks.uiuc.edu>
2007-06-26 17:12:26 -0500

    Change buffer size to 64K when sending Molecule object if the numAtoms > 1M. The buffer size could be set to even a larger value. But this will cause namd to run out of memory on BG/L if compiled with native machine layer. The possible reason is due to the memory fragmentation so that there is no such a big free chunk for a larger buffer size

--- src/Molecule.C
+++ src/Molecule.C
@@ -2290,7 +2290,7 @@ void Molecule::send_Molecule(Communicate *com_obj)
   //  Message to send to clients
   int bufSize = BUFSIZE;
   // When the simulation system is very large, then the buffer size should be expanded to reduce the number of one-to-all broadcasts.
-  if(numAtoms>=1000000) bufSize=4096*1024;
+  if(numAtoms>=1000000) bufSize=16*BUFSIZE;
   MOStream *msg=com_obj->newOutputStream(ALLBUTME, MOLECULETAG, bufSize);
   if ( msg == NULL )
   {
