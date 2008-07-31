de1e976ebe01d4d0ad621993558173fe908dd436
Chao Mei <chaomei2@ks.uiuc.edu>
2008-07-31 15:55:21 -0500

    1. added the initialization for the node-level proxy manager 2. changes the way to enable send/recv proxy spanning tree

--- src/Node.C
+++ src/Node.C
@@ -232,10 +232,10 @@ void Node::startup() {
   break;
 
   case 3:     
-    if(simParameters->enableProxySendST) {
+    if(simParameters->isSendProxySTEnabled()) {
         ProxyMgr::Object()->setSendSpanning();
     }
-    if(simParameters->enableProxyRecvST) {
+    if(simParameters->isRecvProxySTEnabled()) {
         ProxyMgr::Object()->setRecvSpanning();
     }
     #ifdef PROCTRACE_DEBUG
@@ -260,6 +260,21 @@ void Node::startup() {
       registerUserEventsForAllComputeObjs();
 
       workDistrib->sendMaps();
+      #ifdef USE_NODEPATCHMGR
+      CProxy_NodeProxyMgr npm(CpvAccess(BOCclass_group).nodeProxyMgr);
+      //a node broadcast
+      npm.createProxyInfo(PatchMap::Object()->numPatches());
+      #endif
+    }
+    {
+        #if defined(NODEAWARE_PROXY_SPANNINGTREE) && defined(USE_NODEPATCHMGR)
+        CProxy_NodeProxyMgr npm(CpvAccess(BOCclass_group).nodeProxyMgr);
+        if(CkMyRank()==0) {
+            //just need to register once
+            npm[CkMyNode()].ckLocalBranch()->registerLocalProxyMgr(CpvAccess(BOCclass_group).proxyMgr);
+        }
+        npm[CkMyNode()].ckLocalBranch()->registerLocalPatchMap(CkMyRank(), PatchMap::Object());
+        #endif
     }
   break;
 
