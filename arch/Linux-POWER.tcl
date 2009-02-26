d8a183d4d026833be8d2c54b9dd6ddb1d41e06d4
Jim Phillips <jim@ks.uiuc.edu>
2009-02-26 16:44:30 -0600

    Removing redundant and obsolete config files.

--- arch/Linux-POWER.tcl
+++
@@ -1,7 +0,0 @@
-
-TCLDIR=$(HOME)/tcl
-TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.3 -ldl
-TCLFLAGS=-DNAMD_TCL
-TCL=$(TCLINCL) $(TCLFLAGS)
-
--- arch/Linux-i686-redhat9.tcl
+++
@@ -1,7 +0,0 @@
-
-TCLDIR=/Projects/namd2/tcl/linux-redhat9
-TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.3 -ldl
-TCLFLAGS=-DNAMD_TCL
-TCL=$(TCLINCL) $(TCLFLAGS)
-
