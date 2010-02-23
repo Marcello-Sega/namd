7e33cc510d6b1b06fea39e955b09d99ed370a739
Jim Phillips <jim@ks.uiuc.edu>
2010-02-23 13:58:39 -0600

    Make drude follow group parent if hydrogen preceeds parent in input.

--- src/Hydrogen.h
+++ src/Hydrogen.h
@@ -38,15 +38,22 @@ class HydrogenGroupID {
       int mp2 = ( a.isMP ? a.atomID : a.MPID );
       if ( mp1 != mp2 ) {
         rval = ( mp1 < mp2 );
-      } else {
+      } else { // same migration group, compare hydrogen groups
         int gp1 = ( isGP ? atomID : GPID );
         int gp2 = ( a.isGP ? a.atomID : a.GPID );
         if ( gp1 != gp2 ) {
           rval = ( gp1 < gp2 );
+        // in the same group, check for group parent
         } else if ( a.isGP ) {  // compare to self should return 0
           rval = 0;
         } else if ( isGP ) {
           rval = 1;
+        // neither is group parent, check for drude
+        } else if ( a.atomID == gp1 + 1 ) {  // compare to self return 0
+          rval = 0;
+        } else if ( atomID == gp1 + 1 ) {
+          rval = 1;
+        // neither is drude
         } else {
 	  rval = (atomID < a.atomID);
         }
