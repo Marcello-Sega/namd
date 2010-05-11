56482581161ccaffb72efe074ff8cecc826b1935
Jim Phillips <jim@ks.uiuc.edu>
2010-05-11 14:12:14 -0500

    fix spelling error in message

--- src/ParseOptions.C
+++ src/ParseOptions.C
@@ -838,7 +838,7 @@ Bool ParseOptions::set(const ConfigList& clist)
                   iout << iWARN 
 		    << "The following variables were set in the\n";
 		  iout << iWARN 
-		    << "configuation file but were not needed" << "\n" << endi;
+		    << "configuration file but were not needed" << "\n" << endi;
 	       }
 	       iout << iWARN << "   " << data->name << "\n" << endi;
 	    }
