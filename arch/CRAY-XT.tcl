6832e6631a326ab42f18eb2fd70591a4a6775f2d
Jim Phillips <jim@ks.uiuc.edu>
2010-06-16 12:23:17 -0500

    Clean up XT build files.  Recommend using GNU compilers.

--- arch/CRAY-XT.tcl
+++ arch/CRAY-XT.tcl
@@ -1,5 +1,5 @@
-TCLDIR=$(HOME)/tcl/cray-xt4
+TCLDIR=$(HOME)/tcl
 TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.3
+TCLLIB=-L$(TCLDIR)/lib -ltcl8.3 -ldl
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
