bea9406ee9f7a6cfe8f10a638da9851ec2fcf3cd
Jim Phillips <jim@ks.uiuc.edu>
2011-03-18 11:34:04 -0500

    Remove extra includes.

--- src/diffbinpdb.c
+++ src/diffbinpdb.c
@@ -4,9 +4,7 @@
 /*                                                                  */
 /********************************************************************/
 
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/mman.h>
 #include <fcntl.h>
 #include <stdio.h>
 
