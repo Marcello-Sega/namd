91738124d61682b4916789c85e65cdd5295da18c
Jim Phillips <jim@ks.uiuc.edu>
2011-04-27 14:26:10 -0500

    Point at 64-bit lib and add -framework CoreFoundation to fix linkage.

--- arch/MacOSX-x86_64.tcl
+++ arch/MacOSX-x86_64.tcl
@@ -1,7 +1,7 @@
 
-TCLDIR=/Projects/namd2/tcl/macosx-x86
+TCLDIR=/Projects/namd2/tcl/macosx-x86_64
 TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -lnamdtcl8.4
+TCLLIB=-L$(TCLDIR)/lib -lnamdtcl8.4 -framework CoreFoundation
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
 
