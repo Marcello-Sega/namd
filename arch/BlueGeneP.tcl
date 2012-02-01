e9531bc8f59782d2f9e2ab92b5cd6f149458073f
Jim Phillips <jim@ks.uiuc.edu>
2012-01-31 21:48:17 -0600

    Updates to work on Intrepid.

--- arch/BlueGeneP.tcl
+++ arch/BlueGeneP.tcl
@@ -1,7 +1,7 @@
 
-TCLDIR=$(HOME)/bgx/tcl
+TCLDIR=$(HOME)/tcl
 TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.4
+TCLLIB=-L$(TCLDIR)/lib -ltcl8.5 -ldl -lpthread
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
 
