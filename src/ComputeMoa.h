a5127cd08b074b69328cb7f62039b7c4046e0107
Chris Harrison <char@ks.uiuc.edu>
2012-03-19 09:40:49 -0500

    adding MSA_PAGE_SIZE def

--- src/ComputeMoa.h
+++ src/ComputeMoa.h
@@ -30,7 +30,7 @@
 
 #ifdef CHARM_HAS_MSA
 
-#include "msa/msa.h"
+#define MSA_DEFAULT_ENTRIES_PER_PAGE 500;
 
 typedef MSA::MSA1D<double, DefaultEntry<double>,
         MSA_DEFAULT_ENTRIES_PER_PAGE> Moa1Grid;
