84ef02d2c27e66fb33f21873f7b2c4e4edca9d25
Jim Phillips <jim@ks.uiuc.edu>
2014-01-14 15:09:32 -0600

    For WCA FEP properly handle atoms like H, drude, lone pair that lack dispersion term.  (Patch from Lei Huang.)

--- src/ComputeNonbondedFEP.C
+++ src/ComputeNonbondedFEP.C
@@ -33,52 +33,54 @@ inline void fep_vdw_forceandenergies (BigReal A, BigReal B, BigReal r2,
   *alch_vdw_energy_2_Left = 0.;
 
   if(Fep_WCA_repuOn){
-    const BigReal Emin = B*B/(4.0*A);
-    const BigReal Rmin_SQ = powf(2.0*A/B, 1.f/3);
-    const BigReal r2_1 = r2 + (1.-WCA_rcut1)*(1.-WCA_rcut1)*Rmin_SQ;
-    const BigReal r2_2 = r2 + (1.-WCA_rcut2)*(1.-WCA_rcut2)*Rmin_SQ;
-    const BigReal r2_3 = r2 + (1.-WCA_rcut3)*(1.-WCA_rcut3)*Rmin_SQ;
-    const BigReal r6_1 = r2_1*r2_1*r2_1;
-    const BigReal r6_2 = r2_2*r2_2*r2_2;
-    const BigReal r6_3 = r2_3*r2_3*r2_3;
-    const BigReal WCA_rcut1_energy = r2 <= Rmin_SQ * \
+    if(B != 0.0)	{	// Excluded when some atoms like H, drude, lone pair are involved
+       const BigReal Emin = B*B/(4.0*A);
+       const BigReal Rmin_SQ = powf(2.0*A/B, 1.f/3);
+       const BigReal r2_1 = r2 + (1.-WCA_rcut1)*(1.-WCA_rcut1)*Rmin_SQ;
+       const BigReal r2_2 = r2 + (1.-WCA_rcut2)*(1.-WCA_rcut2)*Rmin_SQ;
+       const BigReal r2_3 = r2 + (1.-WCA_rcut3)*(1.-WCA_rcut3)*Rmin_SQ;
+       const BigReal r6_1 = r2_1*r2_1*r2_1;
+       const BigReal r6_2 = r2_2*r2_2*r2_2;
+       const BigReal r6_3 = r2_3*r2_3*r2_3;
+       const BigReal WCA_rcut1_energy = r2 <= Rmin_SQ * \
                                      (1.0 - (1.0-WCA_rcut1)*(1.0-WCA_rcut1))? \
                                      A/(r6_1*r6_1) - B/r6_1 + Emin : 0.;
-    const BigReal WCA_rcut2_energy = r2 <= Rmin_SQ * \
+       const BigReal WCA_rcut2_energy = r2 <= Rmin_SQ * \
                                      (1.0 - (1.0-WCA_rcut2)*(1.0-WCA_rcut2))? \
                                      A/(r6_2*r6_2) - B/r6_2 + Emin : 0.;
-    const BigReal WCA_rcut3_energy = r2 <= Rmin_SQ * \
+       const BigReal WCA_rcut3_energy = r2 <= Rmin_SQ * \
                                      (1.0 - (1.0-WCA_rcut3)*(1.0-WCA_rcut3))? \
                                      A/(r6_3*r6_3) - B/r6_3 + Emin : 0.;
-    const BigReal WCA_rcut1_force = r2 <= Rmin_SQ * \
+       const BigReal WCA_rcut1_force = r2 <= Rmin_SQ * \
                                     (1.0 - (1.0-WCA_rcut1)*(1.0-WCA_rcut1))? \
                                     (12.*(WCA_rcut1_energy) \
                                      + 6.*B/r6_1 - 12.0 * Emin )/r2_1: 0.;
-    const BigReal WCA_rcut2_force = r2 <= Rmin_SQ * \
+       const BigReal WCA_rcut2_force = r2 <= Rmin_SQ * \
                                     (1.0 - (1.0-WCA_rcut2)*(1.0-WCA_rcut2))? \
                                     (12.*(WCA_rcut2_energy) \
                                      + 6.*B/r6_2 - 12.0 * Emin )/r2_2: 0.;
-    const BigReal WCA_rcut3_force = r2 <= Rmin_SQ * \
+       const BigReal WCA_rcut3_force = r2 <= Rmin_SQ * \
                                     (1.0 - (1.0-WCA_rcut3)*(1.0-WCA_rcut3))? \
                                     (12.*(WCA_rcut3_energy) \
                                      + 6.*B/r6_3 - 12.0 * Emin )/r2_3: 0.;
   // separation-shifted repulsion force and energy
-    *alch_vdw_energy = WCA_rcut2_energy; 
-    *alch_vdw_force =  WCA_rcut2_force;
-    if(WCA_rcut1 < WCA_rcut2) {
-      *alch_vdw_energy_2_Left = *alch_vdw_energy + WCA_rcut2_energy - WCA_rcut1_energy; 
+       *alch_vdw_energy = WCA_rcut2_energy; 
+       *alch_vdw_force =  WCA_rcut2_force;
+       if(WCA_rcut1 < WCA_rcut2) {
+         *alch_vdw_energy_2_Left = *alch_vdw_energy + WCA_rcut2_energy - WCA_rcut1_energy; 
+       }
+       if(WCA_rcut2 < WCA_rcut3) {
+         *alch_vdw_energy_2 = *alch_vdw_energy + WCA_rcut3_energy - WCA_rcut2_energy; 
+       }
     }
-    if(WCA_rcut2 < WCA_rcut3) {
-      *alch_vdw_energy_2 = *alch_vdw_energy + WCA_rcut3_energy - WCA_rcut2_energy; 
+    else  {
+    	*alch_vdw_energy = 0.0;
+    	*alch_vdw_force = 0.0;
+    	*alch_vdw_energy_2_Left = 0.0;
+    	*alch_vdw_energy_2 = 0.0;
     }
   }
   else if(Fep_WCA_dispOn){
-    const BigReal Emin = B*B/(4.0*A);
-    const BigReal Rmin_SQ = powf(2.0*A/B, 1.f/3);
-    const BigReal r2_1 = r2;
-    const BigReal r2_2 = r2;
-    const BigReal r6_1 = r2_1*r2_1*r2_1;
-    const BigReal r6_2 = r2_2*r2_2*r2_2;
   // separation-shifted dispersion force and energy
     if(B == 0.0)	{	// some atoms like H, drude, lone pair are involved
 			*alch_vdw_energy = 0.0;
@@ -86,6 +88,12 @@ inline void fep_vdw_forceandenergies (BigReal A, BigReal B, BigReal r2,
 			*alch_vdw_energy_2 = 0.0;
     }
     else {
+      const BigReal Emin = B*B/(4.0*A);
+      const BigReal Rmin_SQ = powf(2.0*A/B, 1.f/3);
+      const BigReal r2_1 = r2;
+      const BigReal r2_2 = r2;
+      const BigReal r6_1 = r2_1*r2_1*r2_1;
+      const BigReal r6_2 = r2_2*r2_2*r2_2;
       *alch_vdw_energy = r2 > Rmin_SQ? \
                          myVdwLambda*(A/(r6_1*r6_1) - B/r6_1): \
                          A/(r6_1*r6_1) - B/r6_1 + (1.-myVdwLambda)* Emin;
