ecf87fd9965683d7f9d66fa7c4adfcb21c2398ab
Jim Phillips <jim@ks.uiuc.edu>
2014-06-18 22:51:04 -0500

    Update Linux POWER port.

--- arch/Linux-POWER.tcl
+++ arch/Linux-POWER.tcl
@@ -1,7 +1,8 @@
 
-TCLDIR=$(HOME)/tcl
+TCLDIR=$(HOME)/tcl-threaded
 TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.5 -ldl
+# TCLLIB=-L$(TCLDIR)/lib -ltcl8.5 -ldl
+TCLLIB=-L$(TCLDIR)/lib -ltcl8.5 -ldl -lpthread
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
 
